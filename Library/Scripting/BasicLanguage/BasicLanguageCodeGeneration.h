6803e8d6bbd96af86c02eeb2abfd05a4cfdd423d
vczh_cp <SND\vczh_cp>
2010-02-18 03:57:06 +0000

    BasicLanguageCodeGeneration file structure

---
+++ Library/Scripting/BasicLanguage/BasicLanguageCodeGeneration.h
@@ -0,0 +1,25 @@
+/***********************************************************************
+Vczh Library++ 3.0
+Developer: ³Âè÷å«(vczh)
+Scripting::BasicLanguage
+
+Classes:
+***********************************************************************/
+
+#ifndef VCZH_SCRIPTING_BASICLANGUAGE_BASICLANGUAGECODEGENERATION
+#define VCZH_SCRIPTING_BASICLANGUAGE_BASICLANGUAGECODEGENERATION
+
+#include "BasicLanguageAnalyzer.h"
+#include "..\BasicIL\BasicILDefinition.h"
+
+namespace vl
+{
+	namespace scripting
+	{
+		namespace basiclanguage
+		{
+		}
+	}
+}
+
+#endif
