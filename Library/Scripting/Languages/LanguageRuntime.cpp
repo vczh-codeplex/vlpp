3e5019fbe38ea6dcf8e167b746aa962a0f5dd7fe
vczh_cp <SND\vczh_cp>
2010-03-02 14:52:35 +0000

    Language provider framework update

--- Library/Scripting/Languages/LanguageRuntime.cpp
+++ Library/Scripting/Languages/LanguageRuntime.cpp
@@ -12,12 +12,18 @@ LanguageAssembly
 
 		LanguageAssembly::LanguageAssembly(Ptr<basicil::BasicIL> _il)
 			:il(_il)
+			,host(0)
 		{
 		}
 
 		Ptr<basicil::BasicIL> LanguageAssembly::GetIL()
 		{
 			return il;
+		}
+
+		LanguageHost* LanguageAssembly::GetHost()
+		{
+			return host;
 		}
 
 /***********************************************************************
@@ -27,6 +33,25 @@ LanguageHost
 		LanguageHost::LanguageHost(int stackSize)
 		{
 			interpretor=new BasicILInterpretor(stackSize);
+		}
+
+		bool LanguageHost::LoadAssembly(Ptr<LanguageAssembly> assembly)
+		{
+			if(assembly->host==this)
+			{
+				return true;
+			}
+			if(!loadedAssemblies.Contains(assembly.Obj()))
+			{
+				interpretor->LoadIL(assembly->il.Obj());
+				loadedAssemblies.Add(assembly);
+				assembly->host=this;
+				return true;
+			}
+			else
+			{
+				return false;
+			}
 		}
 	}
 }
