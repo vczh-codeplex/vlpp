f8bc89a0a92fa1cc29e84f020b018e39fd69a943
vczh_cp <SND\vczh_cp>
2010-03-06 06:45:33 +0000

    Fix bugs in StrGen and use StrGen to generate Library\Scripting\Languages\BasicLanguageMessage.h,.cpp

---
+++ Library/Scripting/Languages/BasicErrorMessage.h
@@ -0,0 +1,62 @@
+/***********************************************************************
+Vczh Library++ 3.0
+Developer: 陈梓瀚(vczh)
+StringResource::BasicErrorMessage
+
+Classes:
+	BasicErrorMessage									：字符串资源类
+	
+本文件使用Vczh String Resource Code Generator工具自动生成
+***********************************************************************/
+
+#ifndef VCZH_STRING_RESOURCE_CODE_FILE_VL_SCRIPTING_BASICLANGUAGE_BASICERRORMESSAGE
+#define VCZH_STRING_RESOURCE_CODE_FILE_VL_SCRIPTING_BASICLANGUAGE_BASICERRORMESSAGE
+
+#include "..\..\String.h"
+
+using namespace vl;
+
+namespace vl
+{
+	namespace scripting
+	{
+		namespace basiclanguage
+		{
+			class BasicErrorMessage
+			{
+			public:
+				static WString TypeNameNotExists(const WString& type);
+				static WString FunctionAlreadyExists(const WString& name);
+				static WString VariableAlreadyExists(const WString& name);
+				static WString TypeAlreadyExists(const WString& name);
+				static WString StructureMemberAlreadyExists(const WString& name);
+				static WString VariableNotExists(const WString& name);
+				static WString FailToCast(const WString& from, const WString& to);
+				static WString VoidFunctionNotHaveResult();
+				static WString GlobalNotHaveResult();
+				static WString CannotInvokeNonFunctionValue(const WString& type);
+				static WString ArgumentNumnerNotMatch();
+				static WString ArgumentTypeNotMatch(const WString& index, const WString& from, const WString& to);
+				static WString StructureMemberNotExists(const WString& name);
+				static WString CannotConvertIndexToInt(const WString& from, const WString& to);
+				static WString CannotSubscribe(const WString& type);
+				static WString UnaryOperandShouldBeLeftValue();
+				static WString UnaryTypeNotMatch(const WString& op, const WString& type);
+				static WString BinaryLeftOperandShouldBeLeftValue(const WString& op);
+				static WString BinaryTypeNotMatch();
+				static WString ConditionCannotConvertToBool(const WString& from, const WString& boolean);
+				static WString BreakShouldBeInLooping(const WString& breakStatement);
+				static WString ContinueShouldBeInLooping(const WString& continueStatement);
+				static WString InitializerTypeNotMatch(const WString& from, const WString& to);
+				static WString ParameterCountNotMatch();
+				static WString ParameterAlreadyExists(const WString& name);
+				static WString StructureMemberCannotBeUndefinedType(const WString& name);
+				static WString LeftOperandShouldBeStructure();
+				static WString LeftOperandShouldBePointerToStructure();
+				static WString PredeclaredStructureShouldBeDefined(const WString& name);
+			};
+		}
+	}
+}
+
+#endif
