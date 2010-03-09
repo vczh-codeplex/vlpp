05504f8214039e8466bebe877529bc71987095f7
Zihan Chen <vczh@163.com>
2010-03-09 17:24:28 +0000

    NativeX parser (0)

--- Library/Scripting/Languages/NativeX/NativeX.cpp
+++ Library/Scripting/Languages/NativeX/NativeX.cpp
@@ -25,6 +25,9 @@ namespace vl
 				Ptr<BasicProgram>	program;
 			};
 
+			typedef Node<TokenInput<RegexToken>, RegexToken>				TokenType;
+			typedef Rule<TokenInput<RegexToken>, Ptr<BasicExpression>>		ExpressionRule;
+
 /***********************************************************************
 ¸¨Öúº¯Êý
 ***********************************************************************/
@@ -35,7 +38,7 @@ namespace vl
 				Ptr<T> node=new T;
 				node->position.start=position.start;
 				node->position.lineStart=position.lineStart;
-				node->position.lineIndex=position.lindIndex;
+				node->position.lineIndex=position.lineIndex;
 				return node;
 			}
 
@@ -46,9 +49,202 @@ namespace vl
 				return node;
 			}
 
+			wchar_t Escape(wchar_t input)
+			{
+				switch(input)
+				{
+				case L'r':
+					return L'\r';
+				case L'n':
+					return L'\n';
+				case L't':
+					return L'\t';
+				default:
+					return input;
+				}
+			}
+
+			wchar_t EscapeWalk(const wchar_t*& input)
+			{
+				if(*input==L'\\')
+				{
+					input++;
+					return Escape(*input++);
+				}
+				else
+				{
+					return *input++;
+				}
+			}
+
+			WString EscapeString(const WString& input)
+			{
+				wchar_t* buffer=new wchar_t[input.Length()+1];
+				const wchar_t* reading=input.Buffer();
+				wchar_t* writing=buffer;
+				while(*reading)
+				{
+					*writing++=EscapeWalk(reading);
+				}
+				*writing=0;
+				WString result=buffer;
+				delete[] buffer;
+				return result;
+			}
+
+			WString ConvertID(const WString& input)
+			{
+				if(input[0]==L'@')
+				{
+					if(input[1]==L'\"')
+					{
+						return EscapeString(input.Sub(2, input.Length()-3));
+					}
+					else
+					{
+						return input.Right(input.Length()-1);
+					}
+				}
+				else
+				{
+					return input;
+				}
+			}
+
 /***********************************************************************
 ÓïÒåº¯Êý
 ***********************************************************************/
+
+			Ptr<BasicExpression> ToTrue(const RegexToken& input)
+			{
+				Ptr<BasicNumericExpression> expression=CreateNode<BasicNumericExpression>(input);
+				expression->type=bool_type;
+				expression->argument.s64=0;
+				expression->argument.bool_value=true;
+				return expression;
+			}
+
+			Ptr<BasicExpression> ToFalse(const RegexToken& input)
+			{
+				Ptr<BasicNumericExpression> expression=CreateNode<BasicNumericExpression>(input);
+				expression->type=bool_type;
+				expression->argument.s64=0;
+				expression->argument.bool_value=true;
+				return expression;
+			}
+
+			Ptr<BasicExpression> ToAChar(const RegexToken& input)
+			{
+				const wchar_t* reading=input.reading+1;
+				Ptr<BasicNumericExpression> expression=CreateNode<BasicNumericExpression>(input);
+				expression->type=char_type;
+				expression->argument.s64=0;
+				expression->argument.char_value=wtoa(WString(EscapeWalk(reading)))[0];
+				return expression;
+			}
+
+			Ptr<BasicExpression> ToWChar(const RegexToken& input)
+			{
+				const wchar_t* reading=input.reading+2;
+				Ptr<BasicNumericExpression> expression=CreateNode<BasicNumericExpression>(input);
+				expression->type=char_type;
+				expression->argument.s64=0;
+				expression->argument.wchar_value=EscapeWalk(reading);
+				return expression;
+			}
+
+			Ptr<BasicExpression> ToAString(const RegexToken& input)
+			{
+				Ptr<BasicMbcsStringExpression> expression=CreateNode<BasicMbcsStringExpression>(input);
+				expression->value=wtoa(EscapeString(WString(input.reading+1, input.length-2)));
+				return expression;
+			}
+
+			Ptr<BasicExpression> ToWString(const RegexToken& input)
+			{
+				Ptr<BasicUnicodeStringExpression> expression=CreateNode<BasicUnicodeStringExpression>(input);
+				expression->value=EscapeString(WString(input.reading+2, input.length-3));
+				return expression;
+			}
+
+			Ptr<BasicExpression> ToInteger(const RegexToken& input)
+			{
+				__int64 number=wtoi64(WString(input.reading, input.length));
+				unsigned __int64 unsignedNumber=wtou64(WString(input.reading, input.length));
+				Ptr<BasicNumericExpression> expression=CreateNode<BasicNumericExpression>(input);
+				expression->argument.s64=0;
+				if(-128<=number && number<=127)
+				{
+					expression->type=s8;
+					expression->argument.s8=(__int8)number;
+				}
+				else if(128<=number && number<=255)
+				{
+					expression->type=u8;
+					expression->argument.u8=(unsigned __int8)number;
+				}
+				else if(-32768<=number && number<32767)
+				{
+					expression->type=s16;
+					expression->argument.s16=(__int16)number;
+				}
+				else if(32768<=number && number<=65535)
+				{
+					expression->type=u16;
+					expression->argument.u16=(unsigned __int16)number;
+				}
+				else if(-(__int64)2147483648<=number && number<=(__int64)2147483647)
+				{
+					expression->type=s32;
+					expression->argument.s32=(__int32)number;
+				}
+				else if((__int64)2147483648<=number && number<=(__int64)4294967295)
+				{
+					expression->type=u32;
+					expression->argument.u32=(unsigned __int32)number;
+				}
+				else if(number<0 || number==unsignedNumber)
+				{
+					expression->type=s64;
+					expression->argument.s64=number;
+				}
+				else
+				{
+					expression->type=u64;
+					expression->argument.u64=unsignedNumber;
+				}
+				return expression;
+			}
+
+			Ptr<BasicExpression> ToFloat(const RegexToken& input)
+			{
+				Ptr<BasicNumericExpression> expression=CreateNode<BasicNumericExpression>(input);
+				expression->type=bool_type;
+				expression->argument.s64=0;
+				expression->argument.f32=(float)wtof(WString(input.reading, input.length));
+				return expression;
+			}
+
+			Ptr<BasicExpression> ToDouble(const RegexToken& input)
+			{
+				Ptr<BasicNumericExpression> expression=CreateNode<BasicNumericExpression>(input);
+				expression->type=bool_type;
+				expression->argument.s64=0;
+				expression->argument.f64=wtof(WString(input.reading, input.length));
+				return expression;
+			}
+
+			Ptr<BasicExpression> ToNull(const RegexToken& input)
+			{
+				return new BasicNullExpression;
+			}
+
+			Ptr<BasicExpression> ToReference(const RegexToken& input)
+			{
+				Ptr<BasicReferenceExpression> expression=CreateNode<BasicReferenceExpression>(input);
+				expression->name=ConvertID(WString(input.reading, input.length));
+				return expression;
+			}
 
 /***********************************************************************
 ´íÎó»Ö¸´
@@ -62,9 +258,46 @@ namespace vl
 			{
 			protected:
 				Ptr<RegexLexer>						lexer;
+
+				TokenType							TRUE;
+				TokenType							FALSE;
+				TokenType							ACHAR;
+				TokenType							WCHAR;
+				TokenType							ASTRING;
+				TokenType							WSTRING;
+				TokenType							INTEGER;
+				TokenType							FLOAT;
+				TokenType							DOUBLE;
+				TokenType							NULLVALUE;
+				TokenType							ID;
+
+				ExpressionRule						primitive;
+				ExpressionRule						reference;
 			public:
 				NativeXParser()
 				{
+					List<WString> tokens;
+					TRUE			= CreateToken(tokens, L"true");
+					FALSE			= CreateToken(tokens, L"false");
+					ACHAR			= CreateToken(tokens, L"\'([^\']|\\\\\\.)\'");
+					WCHAR			= CreateToken(tokens, L"L\'([^\']|\\\\\\.)\'");
+					ASTRING			= CreateToken(tokens, L"\"([^\"]|\\\\\\.)*\"");
+					WSTRING			= CreateToken(tokens, L"L\"([^\"]|\\\\\\.)*\"");
+					INTEGER			= CreateToken(tokens, L"[+/-]?/d+");
+					FLOAT			= CreateToken(tokens, L"[+/-]?/d+(./d+)[fF]");
+					DOUBLE			= CreateToken(tokens, L"[+/-]?/d+./d+");
+					NULLVALUE		= CreateToken(tokens, L"null");
+					ID				= CreateToken(tokens, L"(@?[a-zA-Z_]/w*)|(@\"([^\"]|\\\\\\.)*\")");
+					lexer=new RegexLexer(tokens.Wrap());
+
+					primitive		= TRUE[ToTrue] | FALSE[ToFalse]
+									| ACHAR[ToAChar] | WCHAR[ToWChar]
+									| ASTRING[ToAString] | WSTRING[ToWString]
+									| FLOAT[ToFloat] | DOUBLE[ToDouble]
+									| NULLVALUE[ToNull]
+									| INTEGER[ToInteger]
+									;
+					reference		= ID[ToReference];
 				}
 			};
 
--- Library/String.cpp
+++ Library/String.cpp
@@ -16,6 +16,42 @@ namespace vl
 		return wcstol(string.Buffer(), &endptr, 10);
 	}
 
+	__int64 atoi64(const AString& string)
+	{
+		char* endptr=0;
+		return _strtoi64(string.Buffer(), &endptr, 10);
+	}
+
+	__int64 wtoi64(const WString& string)
+	{
+		wchar_t* endptr=0;
+		return _wcstoi64(string.Buffer(), &endptr, 10);
+	}
+
+	unsigned int atou(const AString& string)
+	{
+		char* endptr=0;
+		return strtoul(string.Buffer(), &endptr, 10);
+	}
+
+	unsigned int wtou(const WString& string)
+	{
+		wchar_t* endptr=0;
+		return wcstoul(string.Buffer(), &endptr, 10);
+	}
+
+	unsigned __int64 atou64(const AString& string)
+	{
+		char* endptr=0;
+		return _strtoui64(string.Buffer(), &endptr, 10);
+	}
+
+	unsigned __int64 wtou64(const WString& string)
+	{
+		wchar_t* endptr=0;
+		return _wcstoui64(string.Buffer(), &endptr, 10);
+	}
+
 	double atof(const AString& string)
 	{
 		char* endptr=0;
@@ -39,6 +75,48 @@ namespace vl
 	{
 		wchar_t buffer[100];
 		_itow_s(number, buffer, sizeof(buffer)/sizeof(*buffer), 10);
+		return buffer;
+	}
+
+	AString i64toa(__int64 number)
+	{
+		char buffer[100];
+		_i64toa_s(number, buffer, sizeof(buffer)/sizeof(*buffer), 10);
+		return buffer;
+	}
+
+	WString i64tow(__int64 number)
+	{
+		wchar_t buffer[100];
+		_i64tow_s(number, buffer, sizeof(buffer)/sizeof(*buffer), 10);
+		return buffer;
+	}
+
+	AString utoa(unsigned int number)
+	{
+		char buffer[100];
+		_ui64toa_s(number, buffer, sizeof(buffer)/sizeof(*buffer), 10);
+		return buffer;
+	}
+
+	WString utow(unsigned int number)
+	{
+		wchar_t buffer[100];
+		_ui64tow_s(number, buffer, sizeof(buffer)/sizeof(*buffer), 10);
+		return buffer;
+	}
+
+	AString u64toa(unsigned __int64 number)
+	{
+		char buffer[100];
+		_ui64toa_s(number, buffer, sizeof(buffer)/sizeof(*buffer), 10);
+		return buffer;
+	}
+
+	WString u64tow(unsigned __int64 number)
+	{
+		wchar_t buffer[100];
+		_ui64tow_s(number, buffer, sizeof(buffer)/sizeof(*buffer), 10);
 		return buffer;
 	}
 
