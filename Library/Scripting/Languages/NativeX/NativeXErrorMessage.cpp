ee651bcde4741a3794bf456d18927934bc893621
Zihan Chen <vczh@163.com>
2010-03-18 17:03:15 +0000

    Finish NativeXProvider main task what is not done yet:   1. Error message/recovery when parsing   2. Translate basic language errors to NativeX error message   3. Programmar executor API for language provider

--- Library/Scripting/Languages/NativeX/NativeXErrorMessage.cpp
+++ Library/Scripting/Languages/NativeX/NativeXErrorMessage.cpp
@@ -11,6 +11,21 @@ namespace vl
 				return L"Unrecognized Token \""+token+L".\"";
 			}
 
+			WString NativeXErrorMessage::UnitAlreadyExists(const WString& name)
+			{
+				return L"Unit "+name+L" already exists.";
+			}
+
+			WString NativeXErrorMessage::UnitNotExists(const WString& name)
+			{
+				return L"Unit "+name+L" not exists.";
+			}
+
+			WString NativeXErrorMessage::UnitCircularReferenced(const WString& name)
+			{
+				return L"Unit "+name+L" has circular unit imports.";
+			}
+
 		}
 	}
 }
