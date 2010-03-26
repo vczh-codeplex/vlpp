2ac2d9367186884c32117fc430e24b07f169c2bc
Zihan Chen <vczh@163.com>
2010-03-26 18:43:32 +0000

    [Lab Project]GUI: First checkin

---
+++ Candidate/GUI/Presentation/Core/GuiNativeWindow.h
@@ -0,0 +1,120 @@
+/***********************************************************************
+Vczh Library++ 3.0
+Developer: ³Âè÷å«(vczh)
+GUI::Native Window
+
+Classes:
+***********************************************************************/
+
+#ifndef VCZH_PRESENTATION_CORE_GUINATIVEWINDOW
+#define VCZH_PRESENTATION_CORE_GUINATIVEWINDOW
+
+#include "GuiTypes.h"
+
+namespace vl
+{
+	namespace presentation
+	{
+
+		class INativeWindow;
+		class INativeWindowListener;
+		class INativeController;
+		class INativeControllerListener;
+
+/***********************************************************************
+Native Window
+***********************************************************************/
+
+		class INativeWindow : private Interface
+		{
+		public:
+			virtual Point				GetLocation()=0;
+			virtual Size				GetSize()=0;
+			virtual Rect				GetBounds()=0;
+			virtual void				SetLocation(Point location)=0;
+			virtual void				SetSize(Size size)=0;
+			virtual void				Move(Point location, Size size)=0;
+			virtual void				Move(const Rect& bounds)=0;
+
+			virtual void				Show()=0;
+			virtual void				ShowMaximized()=0;
+			virtual void				ShowMinimized()=0;
+			virtual void				Hide()=0;
+			virtual bool				IsVisible()=0;
+			virtual void				Enable()=0;
+			virtual void				Disable()=0;
+			virtual bool				IsEnabled()=0;
+			virtual void				SetFocus()=0;
+			virtual bool				IsFocused()=0;
+
+			virtual void				ShowInTaskBar()=0;
+			virtual void				HideInTaskBar()=0;
+			virtual bool				IsAppearedInTaskBar()=0;
+
+			virtual bool				InstallListener(INativeWindowListener* listener)=0;
+			virtual bool				UninstallListener(INativeWindowListener* listener)=0;
+		};
+
+		class INativeWindowListener : private Interface
+		{
+		public:
+			virtual bool				Moving(const Rect& bounds)=0;
+			virtual bool				Moved()=0;
+			virtual void				Enabled()=0;
+			virtual void				Disabled()=0;
+			virtual void				GotFocus()=0;
+			virtual void				LostFocus()=0;
+			virtual void				Opening()=0;
+			virtual void				Opened()=0;
+			virtual void				Closed()=0;
+
+			virtual void				LeftButtonDown(Point position)=0;
+			virtual void				LeftButtonUp(Point position)=0;
+			virtual void				RightButtonDown(Point position)=0;
+			virtual void				RightButtonUp(Point position)=0;
+			virtual void				MiddleButtonDown(Point position)=0;
+			virtual void				MiddleButtonUp(Point position)=0;
+			virtual void				MouseMoving(Point position)=0;
+			virtual void				MouseEntered()=0;
+			virtual void				MouseLeaved()=0;
+
+			virtual void				KeyDown(int code)=0;
+			virtual void				KeyPress(wchar_t code)=0;
+			virtual void				KeyUp(int code)=0;
+		};
+
+/***********************************************************************
+Native Window Provider
+***********************************************************************/
+
+		class INativeController : private Interface
+		{
+		public:
+			virtual INativeWindow*		CreateNativeWindow()=0;
+			virtual void				DestroyNativeWindow(INativeWindow* window)=0;
+
+			virtual bool				InstallListener(INativeControllerListener* listener)=0;
+			virtual bool				UninstallListener(INativeControllerListener* listener)=0;
+		};
+
+		class INativeControllerListener : private Interface
+		{
+		public:
+			virtual void				LeftButtonDown(Point position)=0;
+			virtual void				LeftButtonUp(Point position)=0;
+			virtual void				RightButtonDown(Point position)=0;
+			virtual void				RightButtonUp(Point position)=0;
+			virtual void				MiddleButtonDown(Point position)=0;
+			virtual void				MiddleButtonUp(Point position)=0;
+			virtual void				MouseMoving(Point position)=0;
+			virtual void				MouseEntered()=0;
+			virtual void				MouseLeaved()=0;
+
+			virtual void				KeyDown(int code)=0;
+			virtual void				KeyPress(wchar_t code)=0;
+			virtual void				KeyUp(int code)=0;
+		};
+	}
+}
+
+#endif
