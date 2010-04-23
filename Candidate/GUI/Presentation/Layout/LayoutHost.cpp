08e7d0c5f6003cf6b2bad9e3ae769b5e4d1106ef
vczh_cp <SND\vczh_cp>
2010-04-23 22:44:46 +0000

    [Lab Project]GUI: Finished DockLayout without testing

--- Candidate/GUI/Presentation/Layout/LayoutHost.cpp
+++ Candidate/GUI/Presentation/Layout/LayoutHost.cpp
@@ -127,6 +127,14 @@ LayoutHost
 			return size;
 		}
 
+		Size LayoutHost::GetSuggestedBounds()
+		{
+			Size size;
+			GetBound(margin.left, margin.right, bounds.Width(), size.x);
+			GetBound(margin.top, margin.bottom, bounds.Height(), size.y);
+			return size;
+		}
+
 		void LayoutHost::SetMinSize(Size _size)
 		{
 			if(minSize!=_size)
@@ -277,6 +285,168 @@ FreeLayout
 		collections::IList<LayoutHost*>& FreeLayout::Hosts()
 		{
 			return hosts.Wrap();
+		}
+
+/***********************************************************************
+DockLayout
+***********************************************************************/
+
+		bool DockLayout::DockedHost::operator==(const DockedHost& dockedHost)const
+		{
+			return dockType==dockedHost.dockType && host==dockedHost.host;
+		}
+
+		bool DockLayout::DockedHost::operator!=(const DockedHost& dockedHost)const
+		{
+			return dockType!=dockedHost.dockType || host!=dockedHost.host;
+		}
+
+		int DockLayout::FindHost(LayoutHost* host)
+		{
+			for(int i=0;i<dockedHosts.Count();i++)
+			{
+				if(dockedHosts[i].host==host)
+				{
+					return i;
+				}
+			}
+			return -1;
+		}
+
+		DockLayout::DockLayout()
+		{
+		}
+
+		DockLayout::~DockLayout()
+		{
+		}
+
+		void DockLayout::AdjustHosts()
+		{
+			Rect bounds(Point(), size);
+			for(int i=0;i<dockedHosts.Count();i++)
+			{
+				DockedHost dockedHost=dockedHosts[i];
+				Size suggestedBounds=dockedHost.host->GetSuggestedBounds();
+				switch(dockedHost.dockType)
+				{
+				case Left:
+					{
+						Rect relativeBounds(
+							bounds.x1,
+							bounds.y1,
+							bounds.x1+suggestedBounds.x,
+							bounds.y2);
+						dockedHost.host->SetMarginRelativeBounds(relativeBounds);
+						bounds.x1+=suggestedBounds.x;
+					}
+					break;
+				case Right:
+					{
+						Rect relativeBounds(
+							bounds.x2-suggestedBounds.x,
+							bounds.y1,
+							bounds.x2,
+							bounds.y2);
+						dockedHost.host->SetMarginRelativeBounds(relativeBounds);
+						bounds.x2-=suggestedBounds.x;
+					}
+					break;
+				case Top:
+					{
+						Rect relativeBounds(
+							bounds.x1,
+							bounds.y1,
+							bounds.x2,
+							bounds.y1+suggestedBounds.y);
+						dockedHost.host->SetMarginRelativeBounds(relativeBounds);
+						bounds.y1+=suggestedBounds.y;
+					}
+					break;
+				case Bottom:
+					{
+						Rect relativeBounds(
+							bounds.x1,
+							bounds.y2-suggestedBounds.y,
+							bounds.x2,
+							bounds.y2);
+						dockedHost.host->SetMarginRelativeBounds(relativeBounds);
+						bounds.y2-=suggestedBounds.y;
+					}
+					break;
+				case None:
+					dockedHost.host->SetMarginRelativeBounds(bounds);
+					break;
+				}
+			}
+		}
+
+		void DockLayout::CalculateMinMax()
+		{
+			Size minOffset;
+			Size minResult;
+			Size maxResult(LayoutMaxSize, LayoutMaxSize);
+			for(int i=0;i<dockedHosts.Count();i++)
+			{
+				DockedHost dockedHost=dockedHosts[i];
+				Size minHostSize=dockedHost.host->GetMinBounds()+minOffset;
+				Size maxHostSize=dockedHost.host->GetMaxBounds()+minOffset;
+				
+				if(minResult.x<minHostSize.x)minResult.x=minHostSize.x;
+				if(minResult.y<minHostSize.y)minResult.y=minHostSize.y;
+				if(maxResult.x>maxHostSize.x)maxResult.x=maxHostSize.x;
+				if(maxResult.y>maxHostSize.y)maxResult.y=maxHostSize.y;
+
+				switch(dockedHost.dockType)
+				{
+				case Left:case Right:
+					minOffset.x+=dockedHost.host->GetBounds().Width();
+					break;
+				case Top:case Bottom:
+					minOffset.x+=dockedHost.host->GetBounds().Height();
+					break;
+				}
+			}
+			minSize=minResult;
+			maxSize=maxResult;
+		}
+
+		int DockLayout::GetHostCount()
+		{
+			return dockedHosts.Count();
+		}
+
+		LayoutHost* DockLayout::GetHost(int index)
+		{
+			return dockedHosts[index].host;
+		}
+
+		void DockLayout::DockHost(DockType dockType, LayoutHost* host)
+		{
+			UndockHost(host);
+			DockedHost dockedHost;
+			dockedHost.dockType=dockType;
+			dockedHost.host=host;
+			dockedHosts.Add(dockedHost);
+		}
+
+		void DockLayout::UndockHost(LayoutHost* host)
+		{
+			int index=FindHost(host);
+			if(index!=-1)
+			{
+				dockedHosts.RemoveAt(index);
+			}
+		}
+
+		DockLayout::DockType DockLayout::GetDockType(LayoutHost* host)
+		{
+			return dockedHosts[FindHost(host)].dockType;
+		}
+
+		void DockLayout::ClearHosts()
+		{
+			dockedHosts.Clear();
 		}
 	}
 }
