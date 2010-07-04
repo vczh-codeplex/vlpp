6739f29b8a486dd01a0d6029b7b266a4187e8e3d
Zihan Chen <vczh@163.com>
2010-07-04 08:44:03 +0000

    Test base code for generic

---
+++ UnitTest/UnitTest/TestScripting_BasicIL_Generic.cpp
@@ -0,0 +1,16 @@
+#include "..\..\Library\UnitTest\UnitTest.h"
+#include "..\..\Library\Scripting\BasicIL\BasicILDefinition.h"
+#include "..\..\Library\Scripting\BasicIL\BasicILInterpretor.h"
+#include "..\..\Library\Scripting\BasicIL\BasicILSymbolResource.h"
+
+using namespace vl;
+using namespace vl::scripting;
+using namespace vl::scripting::basicil;
+
+TEST_CASE(TestBasicILInstruction_Generic_Function)
+{
+}
+
+TEST_CASE(TestBasicILInstruction_Generic_FunctionPointer)
+{
+}
