7388d79a9499c99fb1eda7f95361ff56d8b95878
Zihan Chen <vczh@163.com>
2010-09-30 08:18:19 +0000

    [Candidate] temporary checkin ColorizerCodeGenerator

---
+++ Candidate/CodeBoxControl/TokenizerBuilder/ColorizerCodeGenerator.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TokenizerBuilder.Shape;
+
+namespace TokenizerBuilder
+{
+    class ColorizerCodeGenerator
+    {
+        public static string GenerateCSharpCode(ShapeManager manager, string className)
+        {
+            string[] colorIds = null;
+            string[] stateIds = null;
+            int[] charset = null;
+            int[,] transitions = null;
+            bool[] finalStates = null;
+            int[] stateColors = null;
+
+            {
+                StateShape[] states = manager.Shapes[StateShape.StateShapeProprity]
+                    .Cast<StateShape>()
+                    .Where(s => s.Type == StateType.Start)
+                    .Concat(
+                        manager.Shapes[StateShape.StateShapeProprity]
+                        .Cast<StateShape>()
+                        .Where(s => s.Type != StateType.Start)
+                    )
+                    .ToArray();
+
+                ArrowShape[] arrows = manager.Shapes[ArrowShape.ArrowShapeProprity]
+                    .Cast<ArrowShape>()
+                    .ToArray();
+
+                colorIds = manager.Shapes[StateShape.StateShapeProprity]
+                    .Cast<StateShape>()
+                    .Where(s => s.Type == StateType.Finish && !string.IsNullOrEmpty(s.Color))
+                    .Select(s => s.Color)
+                    .ToArray();
+
+                stateIds = states
+                    .Select(s => s.Name)
+                    .ToArray();
+
+                finalStates = states
+                    .Select(s => s.Type == StateType.Finish)
+                    .ToArray();
+
+                stateColors = states
+                    .Select(s => Array.FindIndex(colorIds, x => x == s.Color) + 1)
+                    .ToArray();
+            }
+
+            StringBuilder builder = new StringBuilder();
+            // header
+            builder.AppendLine("using System;");
+            builder.AppendLine("using System.Collections.Generic;");
+            builder.AppendLine("using System.Linq;");
+            builder.AppendLine("using System.Text;");
+            builder.AppendLine("using CodeBoxControl;");
+            builder.AppendLine("using System.Drawing;");
+            builder.AppendLine();
+            builder.AppendLine("namespace " + className + "Namespace");
+            builder.AppendLine("{");
+            builder.AppendLine("    class " + className + " : ITextEditorColorizer");
+            builder.AppendLine("    {");
+
+            // color ids
+            builder.AppendLine("        public const int NormalColorId = 0;");
+            for (int i = 0; i < colorIds.Length; i++)
+            {
+                builder.AppendLine("        public const int " + colorIds[i] + "ColorId = NormalColorId + " + (i + 1).ToString() + ";");
+            }
+            builder.AppendLine();
+
+            // colors
+            builder.AppendLine("        private readonly HighlightColor = Color.FromArgb(173, 214, 255);");
+            builder.AppendLine("        private readonly NormalColor = Color.FromArgb(0, 0, 0);");
+            for (int i = 0; i < colorIds.Length; i++)
+            {
+                builder.AppendLine("        private readonly " + colorIds[i] + "Color = Color.FromArgb(0, 0, 0);");
+            }
+            builder.AppendLine();
+
+            // state ids
+            builder.AppendLine("        private const int StartState = 0;");
+            for (int i = 1; i < stateIds.Length; i++)
+            {
+                builder.AppendLine("        private const int " + stateIds[i] + "StateId = " + i.ToString() + ";");
+            }
+            builder.AppendLine();
+
+            // color items
+            builder.AppendLine("        private TextEditorColorItem[] colorItems = new TextEditorColorItem[" + (colorIds.Length + 1).ToString() + "];");
+            builder.AppendLine("        private int[] charset = new int[" + charset.Length.ToString() + "];");
+            builder.AppendLine("        private int[,] transitions = new int[" + transitions.GetLength(0).ToString() + "," + transitions.GetLength(1).ToString() + "];");
+            builder.AppendLine("        private bool[] finalStates = new bool[" + finalStates.Length.ToString() + "];");
+            builder.AppendLine("        private int[] stateColors = new int[" + stateColors.Length.ToString() + "];");
+            builder.AppendLine();
+            builder.AppendLine("        public TextEditorColorItem[] ColorItems");
+            builder.AppendLine("        {");
+            builder.AppendLine("            get");
+            builder.AppendLine("            {");
+            builder.AppendLine("                return this.colorItems;");
+            builder.AppendLine("            }");
+            builder.AppendLine("        }");
+            builder.AppendLine();
+            builder.AppendLine("        public " + className + "()");
+            builder.AppendLine("        {");
+            builder.AppendLine("            this.colorItems[NormalColorId] = new TextEditorColorItem()");
+            builder.AppendLine("            {");
+            builder.AppendLine("                Text = NormalColor,");
+            builder.AppendLine("                HighlightText = NormalColor,");
+            builder.AppendLine("                Highlight = HighlightColor");
+            builder.AppendLine("            };");
+            for (int i = 0; i < colorIds.Length; i++)
+            {
+                builder.AppendLine("            this.colorItems[" + colorIds[i] + "ColorId] = new TextEditorColorItem()");
+                builder.AppendLine("            {");
+                builder.AppendLine("                Text = " + colorIds[i] + "Color,");
+                builder.AppendLine("                HighlightText = " + colorIds[i] + "Color,");
+                builder.AppendLine("                Highlight = HighlightColor");
+                builder.AppendLine("            };");
+            }
+            builder.AppendLine("            CreateStateMachine();");
+            builder.AppendLine("        }");
+
+            // transition
+            builder.AppendLine("        public int ColorizeLine(char[] items, int length, int initialState, int[] colors)");
+            builder.AppendLine("        {");
+            builder.AppendLine("            int state = initialState;");
+            builder.AppendLine("            int itemStart = 0;");
+            builder.AppendLine("            int lastFinalState = StartState;");
+            builder.AppendLine();
+            builder.AppendLine("            for (int i = 0; i <= length; i++)");
+            builder.AppendLine("            {");
+            builder.AppendLine("                if (i != length)");
+            builder.AppendLine("                {");
+            builder.AppendLine("                    state = transitions[state, items[i]];");
+            builder.AppendLine("                }");
+            builder.AppendLine();
+            builder.AppendLine("                if (i == length || lastFinalState != state && lastFinalState != StartState)");
+            builder.AppendLine("                {");
+            builder.AppendLine("                    int color = stateColors[lastFinalState];");
+            builder.AppendLine("                    for (int j = itemStart; j < i; j++)");
+            builder.AppendLine("                    {");
+            builder.AppendLine("                        colors[j] = color;");
+            builder.AppendLine("                    }");
+            builder.AppendLine("                    itemStart = i;");
+            builder.AppendLine("                }");
+            builder.AppendLine("                lastFinalState = finalStates[state] ? state : StartState;");
+            builder.AppendLine("            }");
+            builder.AppendLine();
+            builder.AppendLine("            return transitions[state, '\n'];");
+            builder.AppendLine("        }");
+            builder.AppendLine();
+
+            // state machine
+            builder.AppendLine("        private void CreateStateMachine()");
+            builder.AppendLine("        {");
+            for (int i = 0; i < charset.Length; i++)
+            {
+                builder.AppendLine("            charset[" + i.ToString() + "] = " + charset[i].ToString() + ";");
+            }
+            builder.AppendLine();
+            for (int i = 0; i < finalStates.Length; i++)
+            {
+                builder.AppendLine("            finalStates[" + i.ToString() + "] = " + finalStates[i].ToString() + ";");
+            }
+            builder.AppendLine();
+            for (int i = 0; i < stateColors.Length; i++)
+            {
+                builder.AppendLine("            stateColors[" + i.ToString() + "] = " + stateColors[i].ToString() + ";");
+            }
+            builder.AppendLine();
+            for (int i = 0; i < transitions.GetLength(0); i++)
+            {
+                for (int j = 0; j < transitions.GetLength(1); j++)
+                {
+                    builder.AppendLine("            stateColors[" + i.ToString() + ", " + j.ToString() + "] = " + transitions[i, j].ToString() + ";");
+                }
+            }
+            builder.AppendLine("        }");
+
+            // final
+            builder.AppendLine("    }");
+            builder.AppendLine("}");
+            return builder.ToString();
+        }
+    }
+}
