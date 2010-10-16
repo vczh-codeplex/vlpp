c5dcfc2dc8ba5ffe3415a157acd84fd0ff7ed221
vczh_cp <SND\vczh_cp>
2010-10-16 07:04:06 +0000

    [Candidate] Parser generator code skeleton for IDE intellisense

--- Candidate/CodeBoxControl/CodeBoxControl/CodeProvider/ParserCodeGenerator/ParserGenerator.cs
+++ Candidate/CodeBoxControl/CodeBoxControl/CodeProvider/ParserCodeGenerator/ParserGenerator.cs
@@ -26,7 +26,9 @@ namespace CodeBoxControl.CodeProvider.ParserCodeGenerator
             {
                 sb.AppendLine("        public static " + rule.NodeType.FullName + " Parse" + rule.RuleName + "(List<CodeToken> tokens, ref int currentToken)");
                 sb.AppendLine("        {");
-                sb.AppendLine("            throw new NotImplementedException();");
+                sb.AppendLine("            " + GetTypeFullName(rule.NodeType) + " result = default(" + GetTypeFullName(rule.NodeType) + ");");
+                sb.Append(RuleCodeGenerator.GenerateCode(rule.NodeType, rule.Content, "result", "            ", 0));
+                sb.AppendLine("            return result;");
                 sb.AppendLine("        }");
                 sb.AppendLine();
             }
@@ -34,6 +36,35 @@ namespace CodeBoxControl.CodeProvider.ParserCodeGenerator
             sb.AppendLine("}");
 
             return sb.ToString();
+        }
+
+        private static string GetTypeFullName(Type type)
+        {
+            if (type.IsGenericTypeDefinition)
+            {
+                string name = type.FullName;
+                return name.Substring(0, name.IndexOf('`'));
+            }
+            else if (type.IsGenericType)
+            {
+                string result = GetTypeFullName(type.GetGenericTypeDefinition()) + "<";
+                Type[] arguments = type.GetGenericArguments();
+                for (int i = 0; i < arguments.Length; i++)
+                {
+                    if (i > 0) result += ", ";
+                    result += GetTypeFullName(arguments[i]);
+                }
+                result += ">";
+                return result;
+            }
+            else if (type.IsArray)
+            {
+                return GetTypeFullName(type.GetElementType()) + "[]";
+            }
+            else
+            {
+                return type.FullName;
+            }
         }
 
         class RuleNodeCollector : IParserNodeVisitor
@@ -101,6 +132,127 @@ namespace CodeBoxControl.CodeProvider.ParserCodeGenerator
             }
 
             #endregion
+        }
+
+        class RuleMemberCollector : IParserNodeVisitor
+        {
+            public List<MemberNode> members = new List<MemberNode>();
+
+            public static List<MemberNode> GetMembers(ParserNode parserNode)
+            {
+                RuleMemberCollector collector = new RuleMemberCollector();
+                parserNode.Accept(collector);
+                return collector.members;
+            }
+
+            #region IParserNodeVisitor Members
+
+            public void Visit(ChoiceNode node)
+            {
+                node.Left.Accept(this);
+                node.Right.Accept(this);
+            }
+
+            public void Visit(LeftRecursionNode node)
+            {
+            }
+
+            public void Visit(ListNode node)
+            {
+                node.Separator.Accept(this);
+                node.Item.Accept(this);
+            }
+
+            public void Visit(MemberNode node)
+            {
+                members.Add(node);
+                node.Content.Accept(this);
+            }
+
+            public void Visit(ReturnNode node)
+            {
+                node.Content.Accept(this);
+            }
+
+            public void Visit(RuleNode node)
+            {
+            }
+
+            public void Visit(SequenceNode node)
+            {
+                node.Left.Accept(this);
+                node.Right.Accept(this);
+            }
+
+            public void Visit(TokenNode node)
+            {
+            }
+
+            public void Visit(TokenContentNode node)
+            {
+            }
+
+            #endregion
+        }
+
+        class RuleCodeGenerator : IParserNodeVisitor
+        {
+            public string returnVariable = "";
+            public string identation = "";
+            public int level = 0;
+            public StringBuilder sb = new StringBuilder();
+
+            public static string GenerateCode(Type nodeType, ParserNode node, string returnVariable, string identation, int level)
+            {
+                RuleCodeGenerator generator = new RuleCodeGenerator();
+                generator.returnVariable = returnVariable;
+                generator.identation = identation;
+                generator.level = level;
+
+                foreach (MemberNode memberNode in RuleMemberCollector.GetMembers(node))
+                {
+                    Type memberType = nodeType.GetProperty(memberNode.Member).PropertyType;
+                    generator.sb.AppendLine(identation + GetTypeFullName(memberType) + " " + memberNode.Member + "Member = default(" + GetTypeFullName(memberType) + ");");
+                }
+                node.Accept(generator);
+                return generator.sb.ToString();
+            }
+
+            public void Visit(ChoiceNode node)
+            {
+            }
+
+            public void Visit(LeftRecursionNode node)
+            {
+            }
+
+            public void Visit(ListNode node)
+            {
+            }
+
+            public void Visit(MemberNode node)
+            {
+            }
+
+            public void Visit(ReturnNode node)
+            {
+            }
+
+            public void Visit(RuleNode node)
+            {
+            }
+
+            public void Visit(SequenceNode node)
+            {
+            }
+
+            public void Visit(TokenNode node)
+            {
+            }
+
+            public void Visit(TokenContentNode node)
+            {
+            }
         }
     }
 }
--- Candidate/CodeBoxControl/CodeBoxControlTest.CodeParser/CodeParserTest.Analyzer.Generated.cs
+++ Candidate/CodeBoxControl/CodeBoxControlTest.CodeParser/CodeParserTest.Analyzer.Generated.cs
@@ -10,32 +10,41 @@ namespace CodeBoxControlTest.CodeParser
     {
         public static CodeBoxControlTest.CodeParser.Expression ParseExpression(List<CodeToken> tokens, ref int currentToken)
         {
-            throw new NotImplementedException();
+            CodeBoxControlTest.CodeParser.Expression result = default(CodeBoxControlTest.CodeParser.Expression);
+            return result;
         }
 
         public static CodeBoxControlTest.CodeParser.Expression ParseTerm(List<CodeToken> tokens, ref int currentToken)
         {
-            throw new NotImplementedException();
+            CodeBoxControlTest.CodeParser.Expression result = default(CodeBoxControlTest.CodeParser.Expression);
+            return result;
         }
 
         public static CodeBoxControlTest.CodeParser.Expression ParseFactor(List<CodeToken> tokens, ref int currentToken)
         {
-            throw new NotImplementedException();
+            CodeBoxControlTest.CodeParser.Expression result = default(CodeBoxControlTest.CodeParser.Expression);
+            return result;
         }
 
         public static CodeBoxControlTest.CodeParser.NumberExpression ParseNumber(List<CodeToken> tokens, ref int currentToken)
         {
-            throw new NotImplementedException();
+            CodeBoxControlTest.CodeParser.NumberExpression result = default(CodeBoxControlTest.CodeParser.NumberExpression);
+            System.Double NumberMember = default(System.Double);
+            return result;
         }
 
         public static CodeBoxControlTest.CodeParser.FunctionExpression ParseFunction(List<CodeToken> tokens, ref int currentToken)
         {
-            throw new NotImplementedException();
+            CodeBoxControlTest.CodeParser.FunctionExpression result = default(CodeBoxControlTest.CodeParser.FunctionExpression);
+            System.String NameMember = default(System.String);
+            CodeBoxControl.CodeProvider.CodeNodeList<CodeBoxControlTest.CodeParser.Expression> ParametersMember = default(CodeBoxControl.CodeProvider.CodeNodeList<CodeBoxControlTest.CodeParser.Expression>);
+            return result;
         }
 
         public static CodeBoxControlTest.CodeParser.Expression ParseBracket(List<CodeToken> tokens, ref int currentToken)
         {
-            throw new NotImplementedException();
+            CodeBoxControlTest.CodeParser.Expression result = default(CodeBoxControlTest.CodeParser.Expression);
+            return result;
         }
 
     }
