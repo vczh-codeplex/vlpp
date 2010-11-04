c8395583c2632101ca2111b04af193e8f72580be
Zihan Chen <vczh@163.com>
2010-11-04 08:35:37 +0000

    [Tools] Increase implementation classes compiling performance

--- Tools/DeveloperFramework/Developer.LanguageServices.NativeX/NativeXCodeParser.Configuration.cs
+++ Tools/DeveloperFramework/Developer.LanguageServices.NativeX/NativeXCodeParser.Configuration.cs
@@ -3,12 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Developer.LanguageProvider;
+using Developer.LanguageServices.NativeX.SyntaxTree;
 
 namespace Developer.LanguageServices.NativeX
 {
     static partial class NativeXCodeParser
     {
-        private static NativeXTokenizer tokenizer = new NativeXTokenizer();
+        private static NativeXTokenizer tokenizer = null;
+
+        static NativeXCodeParser()
+        {
+            tokenizer = new NativeXTokenizer();
+            CodeNode.Preload(typeof(NativeXUnit).Assembly);
+        }
 
         public static List<CodeToken> Tokenize(char[] input)
         {
