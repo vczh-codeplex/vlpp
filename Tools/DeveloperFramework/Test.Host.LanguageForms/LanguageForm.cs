6d8fe17ddb9c6e79128a8cccf4b3e642f5885f20
vczh_cp <SND\vczh_cp>
2010-11-04 06:53:41 +0000

    [Tools] Refactor NativeXForm

---
+++ Tools/DeveloperFramework/Test.Host.LanguageForms/LanguageForm.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Developer.WinFormControls;
+using System.IO;
+using System.ServiceModel;
+using Developer.WinFormControls.Core;
+using Developer.LanguageServices.NativeX;
+
+namespace Test.Host.LanguageForms
+{
+    public partial class LanguageForm : Form
+    {
+        protected ServiceHost ServiceHost { get; private set; }
+        protected ITextEditorColorizer Colorizer { get; set; }
+        protected ITextEditorControlPanel ControlPanel { get; set; }
+
+        public LanguageForm()
+        {
+            InitializeComponent();
+        }
+
+        protected void InitializeLanguageForm()
+        {
+            textEditorBox.Colorizer = this.Colorizer;
+            textEditorBox.ControlPanel = this.ControlPanel;
+            TextEditorService.EditorControl = textEditorBox;
+            this.ServiceHost = new ServiceHost(typeof(TextEditorService));
+            this.ServiceHost.Open();
+        }
+
+        protected virtual string CodeFileName
+        {
+            get
+            {
+                return "";
+            }
+        }
+
+        protected override void OnClosed(EventArgs e)
+        {
+            base.OnClosed(e);
+            this.ServiceHost.Close();
+        }
+
+        protected override void OnShown(EventArgs e)
+        {
+            base.OnShown(e);
+            if (File.Exists(this.CodeFileName))
+            {
+                using (StreamReader reader = new StreamReader(this.CodeFileName))
+                {
+                    textEditorBox.Text = reader.ReadToEnd();
+                }
+            }
+        }
+    }
+}
