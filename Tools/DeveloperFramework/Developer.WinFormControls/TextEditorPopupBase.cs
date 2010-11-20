a1fd54748dd28bc8cc9bf956ab8130831dce5f74
Zihan Chen <vczh@163.com>
2010-11-20 09:40:07 +0000

    [Tools] Add global hook to close popup window

--- Tools/DeveloperFramework/Developer.WinFormControls/TextEditorPopupBase.cs
+++ Tools/DeveloperFramework/Developer.WinFormControls/TextEditorPopupBase.cs
@@ -12,9 +12,12 @@ namespace Developer.WinFormControls
 {
     public partial class TextEditorPopupBase : Form
     {
+        private MouseHook mouseHook = new MouseHook();
+
         public TextEditorPopupBase()
         {
             InitializeComponent();
+            this.mouseHook.OnMouseActivity += new MouseEventHandler(mouseHook_OnMouseActivity);
         }
 
         public void Show(Control control, Point locationTop, Point locationBottom)
@@ -54,8 +57,48 @@ namespace Developer.WinFormControls
             {
                 ShowWindow(this.Handle, 4);//SW_SHOWNOACTIVATE
             }
-            SetWindowPos(this.Handle, 0, 0, 0, 0, 0, 19);//HWND_TOP, SWP_NOMOVE | SWP_MOSIZE | SWP_NOACTIVATE
+            SetWindowPos(this.Handle, -1, 0, 0, 0, 0, 19);//HWND_TOPMOST, SWP_NOMOVE | SWP_MOSIZE | SWP_NOACTIVATE
             this.Location = location;
+            this.Size = size;
+        }
+
+        protected override void OnVisibleChanged(EventArgs e)
+        {
+            base.OnVisibleChanged(e);
+            if (this.Visible)
+            {
+                this.mouseHook.Start();
+            }
+            else
+            {
+                this.mouseHook.Stop();
+            }
+        }
+
+        private void GlobalMouseDown()
+        {
+            if (!new Rectangle(PointToScreen(new Point(0, 0)), this.Size).Contains(Control.MousePosition))
+            {
+                Hide();
+            }
+        }
+
+        private void mouseHook_OnMouseActivity(object sender, MouseEventArgs e)
+        {
+            if (e.Button != MouseButtons.None)
+            {
+                if (this.InvokeRequired)
+                {
+                    this.Invoke(new MethodInvoker(() =>
+                    {
+                        GlobalMouseDown();
+                    }));
+                }
+                else
+                {
+                    GlobalMouseDown();
+                }
+            }
         }
 
         [DllImport("User32.dll")]
