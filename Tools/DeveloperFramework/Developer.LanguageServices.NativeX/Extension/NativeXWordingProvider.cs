3fcd46c854efe6b649552cb546572dbe56cb7098
Zihan Chen <vczh@163.com>
2010-11-20 16:02:37 +0000

    [Tools] Double click and select word

---
+++ Tools/DeveloperFramework/Developer.LanguageServices.NativeX/Extension/NativeXWordingProvider.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Developer.WinFormControls.Extension;
+
+namespace Developer.LanguageServices.NativeX.Extension
+{
+    public class NativeXWordingProvider : LanguageWordingExtension
+    {
+        public class NativeXProvider : ILanguageWordingProvider
+        {
+            protected char[] chars = @"[](){}.,;:!~+-*/%^|&=<>".OrderBy(c => c).ToArray();
+
+            public virtual int GetNonNegativeCharClass(char c)
+            {
+                if ('a' <= c && c <= 'z' || 'A' <= c && c <= 'Z' || '0' <= c && c <= '9' || c == '_')
+                {
+                    return 0;
+                }
+                else if (c == ' ' || c == '\r' || c == '\n' || c == '\t')
+                {
+                    return 1;
+                }
+                else if (Array.BinarySearch(this.chars, c) != -1)
+                {
+                    return 2;
+                }
+                else
+                {
+                    return 3;
+                }
+            }
+        }
+
+        public NativeXWordingProvider()
+            : base(new NativeXProvider())
+        {
+        }
+
+        public NativeXWordingProvider(NativeXProvider provider)
+            : base(provider)
+        {
+        }
+    }
+}
---
+++ Tools/DeveloperFramework/Developer.WinFormControls/Extension/LanguageWordingExtension.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Developer.LanguageProvider;
+using Developer.WinFormControls.Core;
+
+namespace Developer.WinFormControls.Extension
+{
+    public class LanguageWordingExtension : ITextEditorBoxWordingExtension
+    {
+        protected ILanguageWordingProvider Provider { get; private set; }
+
+        public ITextEditorControlPanelCallBack Callback { get; private set; }
+
+        public LanguageWordingExtension(ILanguageWordingProvider provider)
+        {
+            this.Provider = provider;
+        }
+
+        public TextPosition GetLeftWord(TextPosition caret)
+        {
+            return GetWord(Backward(caret), Backward);
+        }
+
+        public TextPosition GetRightWord(TextPosition caret)
+        {
+            return Forward(GetWord(caret, Forward));
+        }
+
+        public void Attach(ITextEditorControlPanelCallBack callback)
+        {
+            this.Callback = callback;
+        }
+
+        private TextPosition Backward(TextPosition caret)
+        {
+            if (caret.col == 0)
+            {
+                if (caret.row == 0)
+                {
+                    return caret;
+                }
+                else
+                {
+                    return new TextPosition(caret.row - 1, this.Callback.TextEditorBox.TextProvider[caret.row - 1].CharCount);
+                }
+            }
+            else
+            {
+                return new TextPosition(caret.row, caret.col - 1);
+            }
+        }
+
+        private TextPosition Forward(TextPosition caret)
+        {
+            if (caret.col == this.Callback.TextEditorBox.TextProvider[caret.row].CharCount)
+            {
+                if (caret.row == this.Callback.TextEditorBox.TextProvider.Count - 1)
+                {
+                    return caret;
+                }
+                else
+                {
+                    return new TextPosition(caret.row + 1, 0);
+                }
+            }
+            else
+            {
+                return new TextPosition(caret.row, caret.col + 1);
+            }
+        }
+
+        private TextPosition GetWord(TextPosition caret, Func<TextPosition, TextPosition> step)
+        {
+            TextPosition end = caret;
+            int charClass = -1;
+            while (true)
+            {
+                int currentCharClass = -1;
+                TextLine<TextEditorBox.LineInfo> line = this.Callback.TextEditorBox.TextProvider[caret.row];
+                if (caret.col < line.CharCount)
+                {
+                    currentCharClass = this.Provider.GetNonNegativeCharClass(line.CharArray[caret.col]);
+                }
+                else
+                {
+                    currentCharClass = this.Provider.GetNonNegativeCharClass('\n');
+                }
+                if (charClass == -1 || charClass == currentCharClass)
+                {
+                    charClass = currentCharClass;
+                    end = caret;
+                    caret = step(caret);
+                    if (caret == end)
+                    {
+                        break;
+                    }
+                }
+                else
+                {
+                    break;
+                }
+            }
+            return end;
+        }
+    }
+
+    public interface ILanguageWordingProvider
+    {
+        int GetNonNegativeCharClass(char c);
+    }
+}
