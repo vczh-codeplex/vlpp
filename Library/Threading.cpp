ecd1a69e898252fad1be146aa6296db3dd8c775d
Zihan Chen <vczh@163.com>
2010-12-19 13:12:15 +0000

    Implement CriticalSection, Mutex and Semaphore

--- Library/Threading.cpp
+++ Library/Threading.cpp
@@ -63,7 +63,7 @@ Thread
 
 	Thread::Thread()
 	{
-		internalData=new ThreadData();
+		internalData=new ThreadData;
 		internalData->threadHandle=CreateThread(NULL, 0, InternalThreadProcWrapper, this, CREATE_SUSPENDED, &internalData->threadId);
 		threadState=Thread::NotStarted;
 	}
@@ -167,5 +167,243 @@ Thread
 	Thread::ThreadState Thread::GetState()
 	{
 		return threadState;
+	}
+
+/***********************************************************************
+CriticalSection
+***********************************************************************/
+
+	namespace threading_internal
+	{
+		struct CriticalSectionData
+		{
+			CRITICAL_SECTION		criticalSection;
+		};
+	}
+			
+	CriticalSection::CriticalSection()
+	{
+		internalData=new CriticalSectionData;
+		InitializeCriticalSection(&internalData->criticalSection);
+	}
+
+	CriticalSection::~CriticalSection()
+	{
+		DeleteCriticalSection(&internalData->criticalSection);
+		delete internalData;
+	}
+
+	bool CriticalSection::TryEnter()
+	{
+		return TryEnterCriticalSection(&internalData->criticalSection)!=0;
+	}
+
+	void CriticalSection::Enter()
+	{
+		EnterCriticalSection(&internalData->criticalSection);
+	}
+
+	void CriticalSection::Leave()
+	{
+		LeaveCriticalSection(&internalData->criticalSection);
+	}
+
+/***********************************************************************
+Mutex
+***********************************************************************/
+
+	namespace threading_internal
+	{
+		struct MutexData
+		{
+			HANDLE				handle;
+
+			MutexData(HANDLE _handle)
+				:handle(_handle)
+			{
+			}
+		};
+	}
+
+	Mutex::Mutex()
+		:internalData(0)
+	{
+	}
+
+	Mutex::~Mutex()
+	{
+		if(internalData)
+		{
+			CloseHandle(internalData->handle);
+			delete internalData;
+		}
+	}
+
+	bool Mutex::Create(bool owned, const WString& name)
+	{
+		if(IsCreated())return false;
+		BOOL aOwned=owned?TRUE:FALSE;
+		LPCTSTR aName=name==L""?NULL:name.Buffer();
+		HANDLE handle=CreateMutex(NULL, aOwned, aName);
+		if(handle)
+		{
+			internalData=new MutexData(handle);
+		}
+		return IsCreated();
+	}
+
+	bool Mutex::Open(bool inheritable, const WString& name)
+	{
+		if(IsCreated())return false;
+		BOOL aInteritable=inheritable?TRUE:FALSE;
+		HANDLE handle=OpenMutex(SYNCHRONIZE, aInteritable, name.Buffer());
+		if(handle)
+		{
+			internalData=new MutexData(handle);
+		}
+		return IsCreated();
+	}
+
+	bool Mutex::IsCreated()
+	{
+		return internalData!=0;
+	}
+
+	bool Mutex::Wait()
+	{
+		return WaitForTime(INFINITE);
+	}
+
+	bool Mutex::WaitForTime(vint ms)
+	{
+		if(IsCreated())
+		{
+			if(WaitForSingleObject(internalData->handle, (DWORD)ms)==WAIT_OBJECT_0)
+			{
+				return true;
+			}
+		}
+		return false;
+	}
+
+	bool Mutex::Release()
+	{
+		if(IsCreated())
+		{
+			return ReleaseMutex(internalData->handle)!=0;
+		}
+		return false;
+	}
+
+/***********************************************************************
+Semaphore
+***********************************************************************/
+
+	namespace threading_internal
+	{
+		struct SemaphoreData
+		{
+			HANDLE				handle;
+
+			SemaphoreData(HANDLE _handle)
+				:handle(_handle)
+			{
+			}
+		};
+	}
+
+	Semaphore::Semaphore()
+		:internalData(0)
+	{
+	}
+
+	Semaphore::~Semaphore()
+	{
+		if(internalData)
+		{
+			CloseHandle(internalData->handle);
+			delete internalData;
+		}
+	}
+
+	bool Semaphore::Create(vint initialCount, vint maxCount, const WString& name)
+	{
+		if(IsCreated())return false;
+		LONG aInitial=(LONG)initialCount;
+		LONG aMax=(LONG)maxCount;
+		LPCTSTR aName=name==L""?NULL:name.Buffer();
+		HANDLE handle=CreateSemaphore(NULL, aInitial, aMax, aName);
+		if(handle)
+		{
+			internalData=new SemaphoreData(handle);
+		}
+		return IsCreated();
+	}
+
+	bool Semaphore::Open(bool inheritable, const WString& name)
+	{
+		if(IsCreated())return false;
+		BOOL aInteritable=inheritable?TRUE:FALSE;
+		HANDLE handle=OpenSemaphore(SYNCHRONIZE, aInteritable, name.Buffer());
+		if(handle)
+		{
+			internalData=new SemaphoreData(handle);
+		}
+		return IsCreated();
+	}
+
+	bool Semaphore::IsCreated()
+	{
+		return internalData!=0;
+	}
+
+	bool Semaphore::Wait()
+	{
+		return WaitForTime(INFINITE);
+	}
+
+	bool Semaphore::WaitForTime(vint ms)
+	{
+		if(IsCreated())
+		{
+			if(WaitForSingleObject(internalData->handle, (DWORD)ms)==WAIT_OBJECT_0)
+			{
+				return true;
+			}
+		}
+		return false;
+	}
+
+	bool Semaphore::Release()
+	{
+		if(IsCreated())
+		{
+			return Release(1)!=-1;
+		}
+		return false;
+	}
+
+	vint Semaphore::Release(vint count)
+	{
+		if(IsCreated())
+		{
+			LONG previous=-1;
+			if(ReleaseSemaphore(internalData->handle, (LONG)count, &previous)!=0)
+			{
+				return (vint)previous;
+			}
+		}
+		return false;
+	}
+
+/***********************************************************************
+EventObject
+***********************************************************************/
+
+	namespace threading_internal
+	{
+		struct EventData
+		{
+		};
 	}
 }
