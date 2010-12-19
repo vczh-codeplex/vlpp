6262ff300456c192c8210f2a6e961e727b67839b
Zihan Chen <vczh@163.com>
2010-12-19 11:49:58 +0000

    Add test cases

--- UnitTest/UnitTest/TestThread.cpp
+++ UnitTest/UnitTest/TestThread.cpp
@@ -17,6 +17,7 @@ namespace mynamespace
 
 	void SimpleThreadProc(Thread* thread, void* argument)
 	{
+		Thread::Sleep(1000);
 		((ThreadData*)argument)->modified=true;
 	}
 }
@@ -26,7 +27,46 @@ TEST_CASE(TestSimpleThread)
 {
 	ThreadData data;
 	Thread* thread=Thread::CreateAndStart(SimpleThreadProc, &data, false);
-	thread->Wait();
+	TEST_ASSERT(thread->GetState()==Thread::Running);
+	TEST_ASSERT(thread->Wait()==true);
+	TEST_ASSERT(thread->GetState()==Thread::Stopped);
+	delete thread;
+	TEST_ASSERT(data.modified==true);
+}
+
+TEST_CASE(TestPauseAndStopThread)
+{
+	{
+		ThreadData data;
+		Thread* thread=Thread::CreateAndStart(SimpleThreadProc, &data, false);
+		TEST_ASSERT(thread->GetState()==Thread::Running);
+		TEST_ASSERT(thread->Pause()==true);
+		TEST_ASSERT(thread->GetState()==Thread::Paused);
+		delete thread;
+		TEST_ASSERT(data.modified==false);
+	}
+	{
+		ThreadData data;
+		Thread* thread=Thread::CreateAndStart(SimpleThreadProc, &data, false);
+		TEST_ASSERT(thread->GetState()==Thread::Running);
+		TEST_ASSERT(thread->Stop()==true);
+		TEST_ASSERT(thread->GetState()==Thread::Stopped);
+		delete thread;
+		TEST_ASSERT(data.modified==false);
+	}
+}
+
+TEST_CASE(TestPauseAndResumeThread)
+{
+	ThreadData data;
+	Thread* thread=Thread::CreateAndStart(SimpleThreadProc, &data, false);
+	TEST_ASSERT(thread->GetState()==Thread::Running);
+	TEST_ASSERT(thread->Pause()==true);
+	TEST_ASSERT(thread->GetState()==Thread::Paused);
+	TEST_ASSERT(thread->Resume()==true);
+	TEST_ASSERT(thread->GetState()==Thread::Running);
+	TEST_ASSERT(thread->Wait()==true);
+	TEST_ASSERT(thread->GetState()==Thread::Stopped);
 	delete thread;
 	TEST_ASSERT(data.modified==true);
 }
