c1b794b4984a23be8fe0c7149a250243f73e1037
vczh_cp <SND\vczh_cp>
2011-02-19 05:09:10 +0000

    Check-in an empty release project (vle.exe for the first release)

---
+++ Tools/Release/VleSource/Vle/VleMain.cpp
@@ -0,0 +1,10 @@
+#include "..\..\..\..\Library\Basic.h"
+#include "..\..\..\..\Library\Console.h"
+
+using namespace vl;
+using namespace vl::console;
+
+int wmain(vint argc , wchar_t* args[])
+{
+	return 0;
+}
