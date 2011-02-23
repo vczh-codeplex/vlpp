2e02ef876602069f876a38221cbddab69daa9556
vczh_cp <SND\vczh_cp>
2011-02-23 15:27:51 +0000

    Add NativeXCodeEditor(intellisense) from the IDE research project

---
+++ Tools/Release/VleSource/VlTurtle/EditorControls/NativeX/NativeXCodeEditor.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using Developer.LanguageServices.NativeX;
+using Developer.WinFormControls;
+
+namespace VlTurtle.EditorControls.NativeX
+{
+    public partial class NativeXCodeEditor : TextEditorBox
+    {
+        public NativeXCodeEditor()
+        {
+            InitializeComponent();
+            this.Colorizer = new NativeXColorizer();
+            this.ControlPanel = new NativeXControlPanel();
+            if (this.components == null)
+            {
+                this.components = new Container();
+            }
+            this.components.Add((NativeXControlPanel)this.ControlPanel);
+        }
+    }
+}
