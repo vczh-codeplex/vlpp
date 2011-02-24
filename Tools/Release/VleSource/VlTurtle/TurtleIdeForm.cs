efe07112b8a5353e2fbb3a54926465f5d71ec295
vczh_cp <SND\vczh_cp>
2011-02-24 13:25:54 +0000

    IDE Cut/Copy/Paste/Redo/Undo

--- Tools/Release/VleSource/VlTurtle/TurtleIdeForm.cs
+++ Tools/Release/VleSource/VlTurtle/TurtleIdeForm.cs
@@ -12,6 +12,11 @@ namespace VlTurtle
     public partial class TurtleIdeForm : Developer.RibbonFramework.RibbonAeroForm
     {
         private TurtleIdeFormContent content = null;
+        private RibbonButton buttonCut;
+        private RibbonButton buttonCopy;
+        private RibbonButton buttonPaste;
+        private RibbonButton buttonUndo;
+        private RibbonButton buttonRedo;
 
         public TurtleIdeForm()
         {
@@ -19,6 +24,19 @@ namespace VlTurtle
             InitializeComponent();
             InitializeRibbon(toolStripRibbon, content);
             CreateRibbon();
+
+            this.content.ButtonStateUpdated += new EventHandler(content_ButtonStateUpdated);
+            this.content.LoadDefaultCode();
+        }
+
+        private void content_ButtonStateUpdated(object sender, EventArgs e)
+        {
+            this.buttonCut.Enabled = this.content.AvailableCut();
+            this.buttonCopy.Enabled = this.content.AvailableCopy();
+            this.buttonPaste.Enabled = this.content.AvailablePaste();
+            this.buttonUndo.Enabled = this.content.AvailableUndo();
+            this.buttonRedo.Enabled = this.content.AvailableRedo();
+            UpdateRibbon(true);
         }
 
         private void CreateRibbon()
@@ -49,6 +67,7 @@ namespace VlTurtle
                             button.BigImage = TurtleIdeRibbon.New;
                             button.SmallImage = TurtleIdeRibbon.New;
                             button.Name = "New";
+                            button.Executed += (s, e) => this.content.OperationNew();
                             panel.ControlItems.Add(button);
                         }
                     }
@@ -61,6 +80,7 @@ namespace VlTurtle
                             button.BigImage = TurtleIdeRibbon.Open;
                             button.SmallImage = TurtleIdeRibbon.Open;
                             button.Name = "Open";
+                            button.Executed += (s, e) => this.content.OperationOpen();
                             panel.ControlItems.Add(button);
                         }
                         {
@@ -68,6 +88,7 @@ namespace VlTurtle
                             button.BigImage = TurtleIdeRibbon.Save;
                             button.SmallImage = TurtleIdeRibbon.Save;
                             button.Name = "Save";
+                            button.Executed += (s, e) => this.content.OperationSave();
                             panel.ControlItems.Add(button);
                         }
                         {
@@ -75,6 +96,7 @@ namespace VlTurtle
                             button.BigImage = TurtleIdeRibbon.SaveAs;
                             button.SmallImage = TurtleIdeRibbon.SaveAs;
                             button.Name = "Save As";
+                            button.Executed += (s, e) => this.content.OperationSaveAs();
                             panel.ControlItems.Add(button);
                         }
                     }
@@ -88,24 +110,27 @@ namespace VlTurtle
                         panel.Policy = RibbonControlSizingPolicy.BigSmall;
                         group.Panels.Add(panel);
                         {
-                            RibbonButton button = new RibbonButton();
-                            button.BigImage = TurtleIdeRibbon.Run;
-                            button.SmallImage = TurtleIdeRibbon.Run;
-                            button.Name = "Run";
+                            RibbonButton button = this.buttonCut = new RibbonButton();
+                            button.BigImage = TurtleIdeRibbon.Cut;
+                            button.SmallImage = TurtleIdeRibbon.Cut;
+                            button.Name = "Cut";
+                            button.Executed += (s, e) => this.content.OperationCut();
                             panel.ControlItems.Add(button);
                         }
                         {
-                            RibbonButton button = new RibbonButton();
-                            button.BigImage = TurtleIdeRibbon.Stop;
-                            button.SmallImage = TurtleIdeRibbon.Stop;
-                            button.Name = "Stop";
+                            RibbonButton button = this.buttonCopy = new RibbonButton();
+                            button.BigImage = TurtleIdeRibbon.Copy;
+                            button.SmallImage = TurtleIdeRibbon.Copy;
+                            button.Name = "Copy";
+                            button.Executed += (s, e) => this.content.OperationCopy();
                             panel.ControlItems.Add(button);
                         }
                         {
-                            RibbonButton button = new RibbonButton();
+                            RibbonButton button = this.buttonPaste = new RibbonButton();
                             button.BigImage = TurtleIdeRibbon.Paste;
                             button.SmallImage = TurtleIdeRibbon.Paste;
                             button.Name = "Paste";
+                            button.Executed += (s, e) => this.content.OperationPaste();
                             panel.ControlItems.Add(button);
                         }
                     }
@@ -115,15 +140,17 @@ namespace VlTurtle
                     group.Name = "Edit";
                     tab.TabPanel.Groups.Add(group);
                     {
-                        RibbonButton button = new RibbonButton();
+                        RibbonButton button = this.buttonUndo = new RibbonButton();
                         button.BigImage = TurtleIdeRibbon.Undo;
                         button.Name = "Undo";
+                        button.Executed += (s, e) => this.content.OperationUndo();
                         group.BigItems.Add(button);
                     }
                     {
-                        RibbonButton button = new RibbonButton();
+                        RibbonButton button = this.buttonRedo = new RibbonButton();
                         button.BigImage = TurtleIdeRibbon.Redo;
                         button.Name = "Redo";
+                        button.Executed += (s, e) => this.content.OperationRedo();
                         group.BigItems.Add(button);
                     }
                 }
@@ -135,12 +162,14 @@ namespace VlTurtle
                         RibbonButton button = new RibbonButton();
                         button.BigImage = TurtleIdeRibbon.Run;
                         button.Name = "Run";
+                        button.Executed += (s, e) => this.content.OperationRun();
                         group.BigItems.Add(button);
                     }
                     {
                         RibbonButton button = new RibbonButton();
                         button.BigImage = TurtleIdeRibbon.Stop;
                         button.Name = "Stop";
+                        button.Executed += (s, e) => this.content.OperationStop();
                         group.BigItems.Add(button);
                     }
                 }
@@ -155,29 +184,34 @@ namespace VlTurtle
                 RibbonMenuButton button = new RibbonMenuButton();
                 button.SmallImage = TurtleIdeRibbon.New;
                 button.Name = "New Turtle";
+                button.Executed += (s, e) => this.content.OperationNew();
                 menu.MenuItems.Add(button);
             }
             {
                 RibbonMenuButton button = new RibbonMenuButton();
                 button.SmallImage = TurtleIdeRibbon.Open;
                 button.Name = "Open Turtle";
+                button.Executed += (s, e) => this.content.OperationOpen();
                 menu.MenuItems.Add(button);
             }
             {
                 RibbonMenuButton button = new RibbonMenuButton();
                 button.SmallImage = TurtleIdeRibbon.Save;
                 button.Name = "Save Turtle";
+                button.Executed += (s, e) => this.content.OperationSave();
                 menu.MenuItems.Add(button);
             }
             {
                 RibbonMenuButton button = new RibbonMenuButton();
                 button.SmallImage = TurtleIdeRibbon.SaveAs;
                 button.Name = "Save Turtle As";
+                button.Executed += (s, e) => this.content.OperationSaveAs();
                 menu.MenuItems.Add(button);
             }
             {
                 RibbonMenuButton button = new RibbonMenuButton();
                 button.Name = "Exit";
+                button.Executed += (s, e) => this.Close();
                 menu.MenuItems.Add(button);
             }
             return menu;
