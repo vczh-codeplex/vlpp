59255200e5ed129e72a34a9ab64a25205f11eb9d
vczh_cp <SND\vczh_cp>
2011-02-26 10:27:03 +0000

    Skeleton of vlscript.dll

--- Tools/Release/VleSource/VlScript/VlScript.cpp
+++ Tools/Release/VleSource/VlScript/VlScript.cpp
@@ -1,13 +1,106 @@
 #include "VlScript.h"
+#include "..\..\..\..\Library\Scripting\Languages\LanguageRuntime.h"
+
+using namespace vl;
+using namespace vl::collections;
+using namespace vl::scripting;
+
+struct VlsHost
+{
+	Ptr<LanguageHost>			host;
+};
+
+struct VlsState
+{
+	Ptr<LanguageState>			state;
+};
+
+struct VlsAssembly
+{
+	Ptr<LanguageAssembly>		assembly;
+	List<WString>				basicFunctionNames;
+	List<int>					basicFunctionEntries;
+};
 
 extern "C"
 {
-	VLSCRIPT_API VlsHost* __stdcall VlsCreateHost()
+	VLSCRIPT_API int __stdcall VlsCreateHost(VlsHost** host)
 	{
-		return 0;
+		return VLS_ERR;
 	}
 
-	VLSCRIPT_API void __stdcall VlsDisposeHost(VlsHost* host)
+	VLSCRIPT_API int __stdcall VlsDisposeHost(VlsHost* host)
 	{
+		return VLS_ERR;
+	}
+
+	VLSCRIPT_API int __stdcall VlsCreateState(VlsHost* host, VlsState** state)
+	{
+		return VLS_ERR;
+	}
+
+	VLSCRIPT_API int __stdcall VlsDisposeState(VlsState* state)
+	{
+		return VLS_ERR;
+	}
+
+	VLSCRIPT_API int __stdcall VlsCreateAssemblyFromFile(wchar_t* fileName, VlsAssembly** assembly)
+	{
+		return VLS_ERR;
+	}
+
+	VLSCRIPT_API int __stdcall VlsDisposeAssembly(VlsAssembly* assembly)
+	{
+		return VLS_ERR;
+	}
+
+	VLSCRIPT_API int __stdcall VlsGetLastHostError(VlsHost* host, wchar_t** error)
+	{
+		return VLS_ERR;
+	}
+
+	VLSCRIPT_API int __stdcall VlsGetBasicFunctionCount(VlsAssembly* assembly, int** count)
+	{
+		return VLS_ERR;
+	}
+
+	VLSCRIPT_API int __stdcall VlsGetBasicFunctionName(VlsAssembly* assembly, int index, wchar_t** name)
+	{
+		return VLS_ERR;
+	}
+
+	VLSCRIPT_API int __stdcall VlsGetBasicFunctionEntry(VlsAssembly* assembly, int index, int* entry)
+	{
+		return VLS_ERR;
+	}
+
+	VLSCRIPT_API int __stdcall VlsRegisterForeignFunction(VlsHost* host, wchar_t* name, VlsForeignFunction* function, void* userData)
+	{
+		return VLS_ERR;
+	}
+
+	VLSCRIPT_API int __stdcall VlsLoadAssembly(VlsHost* host, VlsAssembly* assembly)
+	{
+		return VLS_ERR;
+	}
+
+	VLSCRIPT_API int __stdcall VlsInitAssembly(VlsState* state, VlsAssembly* assembly)
+	{
+		return VLS_ERR;
+	}
+
+	VLSCRIPT_API int __stdcall VlsPush(VlsState* state, void* data, int size)
+	{
+		return VLS_ERR;
+	}
+
+	VLSCRIPT_API int __stdcall VlsSetFunction(VlsState* state, VlsAssembly* assembly, int entry, void* resultAddress)
+	{
+		return VLS_ERR;
+	}
+
+	VLSCRIPT_API int __stdcall VlsExecute(VlsState* state)
+	{
+		return VLS_ERR;
 	}
 }
--- Tools/Release/VleSource/VlScript/VlScript.h
+++ Tools/Release/VleSource/VlScript/VlScript.h
@@ -8,11 +8,40 @@
 #endif
 
 struct VlsHost;
+struct VlsState;
+struct VlsAssembly;
+typedef int (__stdcall VlsForeignFunction)(void* result, void* arguments, void* userData);
 
 extern "C"
 {
-	VLSCRIPT_API VlsHost* __stdcall VlsCreateHost();
-	VLSCRIPT_API void __stdcall VlsDisposeHost(VlsHost* host);
+	const int VLS_OK = 1;
+	const int VLS_ERR = 0;
+
+	VLSCRIPT_API int __stdcall VlsCreateHost(VlsHost** host);
+	VLSCRIPT_API int __stdcall VlsDisposeHost(VlsHost* host);
+	VLSCRIPT_API int __stdcall VlsCreateState(VlsHost* host, VlsState** state);
+	VLSCRIPT_API int __stdcall VlsDisposeState(VlsState* state);
+	VLSCRIPT_API int __stdcall VlsCreateAssemblyFromFile(wchar_t* fileName, VlsAssembly** assembly);
+	VLSCRIPT_API int __stdcall VlsDisposeAssembly(VlsAssembly* assembly);
+	VLSCRIPT_API int __stdcall VlsGetLastHostError(VlsHost* host, wchar_t** error);
+
+	//--------------------------------------------------------------------------------
+
+	VLSCRIPT_API int __stdcall VlsGetBasicFunctionCount(VlsAssembly* assembly, int** count);
+	VLSCRIPT_API int __stdcall VlsGetBasicFunctionName(VlsAssembly* assembly, int index, wchar_t** name);
+	VLSCRIPT_API int __stdcall VlsGetBasicFunctionEntry(VlsAssembly* assembly, int index, int* entry);
+
+	//--------------------------------------------------------------------------------
+
+	VLSCRIPT_API int __stdcall VlsRegisterForeignFunction(VlsHost* host, wchar_t* name, VlsForeignFunction* function, void* userData);
+
+	//--------------------------------------------------------------------------------
+
+	VLSCRIPT_API int __stdcall VlsLoadAssembly(VlsHost* host, VlsAssembly* assembly);
+	VLSCRIPT_API int __stdcall VlsInitAssembly(VlsState* state, VlsAssembly* assembly);
+	VLSCRIPT_API int __stdcall VlsPush(VlsState* state, void* data, int size);
+	VLSCRIPT_API int __stdcall VlsSetFunction(VlsState* state, VlsAssembly* assembly, int entry, void* resultAddress);
+	VLSCRIPT_API int __stdcall VlsExecute(VlsState* state);
 }
 
 #endif
