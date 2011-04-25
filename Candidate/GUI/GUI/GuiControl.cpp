637e0a902b4907a5236e6fdf94713ba6114d7743
Zihan Chen <vczh@163.com>
2011-04-25 07:56:55 +0000

    Architecture of GuiApplication and GuiControl

--- Candidate/GUI/GUI/GuiControl.cpp
+++ Candidate/GUI/GUI/GuiControl.cpp
@@ -4,5 +4,235 @@ namespace vl
 {
 	namespace presentation
 	{
+
+/***********************************************************************
+GuiControl::Grid
+***********************************************************************/
+
+		GuiControl::Grid::Grid()
+			:ownedControl(0)
+		{
+		}
+
+		GuiControl::Grid::~Grid()
+		{
+		}
+
+		GuiControl* GuiControl::Grid::GetOwnedControl()
+		{
+			return ownedControl;
+		}
+
+		bool GuiControl::Grid::SetOwnedControl(GuiControl* control)
+		{
+			if(ownedControl)
+			{
+				ownedControl=control;
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
+
+/***********************************************************************
+GuiControl
+***********************************************************************/
+
+		void GuiControl::NotifySetParent(GuiControl* value)
+		{
+			if(parent)
+			{
+				GuiWindowBase* parentWindow=dynamic_cast<GuiWindowBase*>(parent);
+				if(parentWindow)
+				{
+					NotifyAttachedToWindow(0);
+				}
+			}
+			parent=value;
+			if(parent)
+			{
+				GuiWindowBase* parentWindow=dynamic_cast<GuiWindowBase*>(parent);
+				if(parentWindow)
+				{
+					NotifyAttachedToWindow(parentWindow);
+				}
+			}
+		}
+
+		void GuiControl::NotifyAttachedToWindow(GuiWindowBase* window)
+		{
+			skin=0;
+			if(window)
+			{
+				IGuiSkinBuilder* builder=GetCurrentApplication()->GetSkinBuilder(GetSkinBuilderName());
+				if(builder)
+				{
+					skin=builder->Build(window->GetContainingNativeWindow());
+					skin->SetBounds(bounds);
+				}
+			}
+			if(container)
+			{
+				int count=container->GetChildCount();
+				for(int i=0;i<count;i++)
+				{
+					GuiControl* child=container->GetChild(i);
+					child->NotifyAttachedToWindow(window);
+				}
+			}
+		}
+
+		void GuiControl::NotifyChildEntering(GuiControl* value)
+		{
+		}
+
+		void GuiControl::NotifyChildEntered(GuiControl* value)
+		{
+			if(skin)
+			{
+				skin->InsertChild(container->GetChildIndex(value), value->skin.Obj());
+			}
+		}
+
+		void GuiControl::NotifyChildLeaving(GuiControl* value)
+		{
+			if(skin)
+			{
+				skin->RemoveChild(value->skin.Obj());
+			}
+		}
+
+		void GuiControl::NotifyChildLeaved(GuiControl* value)
+		{
+		}
+
+		void GuiControl::NotifyMoving(Rect& value)
+		{
+		}
+
+		void GuiControl::NotifyMoved(Rect value)
+		{
+		}
+
+		GuiControl::GuiControl()
+			:parent(0)
+		{
+		}
+
+		GuiControl::~GuiControl()
+		{
+		}
+
+		GuiControl* GuiControl::GetParent()
+		{
+			return parent;
+		}
+
+		const WString& GuiControl::GetControlName()
+		{
+			return name;
+		}
+
+		void GuiControl::SetControlName(const WString& value)
+		{
+			name=value;
+		}
+
+		GuiControl::Grid* GuiControl::GetContainer()
+		{
+			return container.Obj();
+		}
+
+		void GuiControl::SetContainer(Grid* value)
+		{
+			if(value->SetOwnedControl(this))
+			{
+				if(container)
+				{
+					int count=container->GetChildCount();
+					for(int i=0;i<count;i++)
+					{
+						GuiControl* child=container->GetChild(i);
+						NotifyChildLeaving(child);
+						child->NotifySetParent(0);
+						NotifyChildLeaved(child);
+					}
+				}
+				container=value;
+				if(container)
+				{
+					int count=container->GetChildCount();
+					for(int i=0;i<count;i++)
+					{
+						GuiControl* child=container->GetChild(i);
+						NotifyChildEntering(child);
+						child->NotifySetParent(this);
+						NotifyChildEntered(child);
+					}
+				}
+			}
+		}
+
+		Rect GuiControl::GetBounds()
+		{
+			return bounds;
+		}
+
+		void GuiControl::SetBounds(Rect value)
+		{
+			NotifyMoving(value);
+			NotifyMoved(value);
+			bounds=value;
+			if(skin)
+			{
+				skin->SetBounds(bounds);
+			}
+		}
+
+/***********************************************************************
+GuiApplication
+***********************************************************************/
+
+		GuiApplication::GuiApplication()
+		{
+		}
+
+		GuiApplication::~GuiApplication()
+		{
+		}
+
+		void GuiApplication::SetSkinBuilder(const WString& name, IGuiSkinBuilder* builder)
+		{
+			skinBuilders.Set(name, builder);
+		}
+
+		IGuiSkinBuilder* GuiApplication::GetSkinBuilder(const WString& name)
+		{
+			int index=skinBuilders.Keys().IndexOf(name);
+			return index==-1?0:skinBuilders.Values()[index].Obj();
+		}
+
+		GuiApplication* currentApplication=0;
+
+		GuiApplication* GetCurrentApplication()
+		{
+			return currentApplication;
+		}
 	}
+}
+
+/***********************************************************************
+NativeMain
+***********************************************************************/
+
+using namespace vl;
+using namespace vl::presentation;
+
+void NativeMain()
+{
+	GuiApplication application;
+	currentApplication=&application;
+	GuiMain();
 }
--- Candidate/GUI/GUI/GuiControl.h
+++ Candidate/GUI/GUI/GuiControl.h
@@ -14,37 +14,121 @@ Interfaces:
 #include "..\..\..\Library\Event.h"
 #include "..\..\..\Library\Collections\List.h"
 #include "..\..\..\Library\Collections\Dictionary.h"
+#include "NativeWindow\GuiNativeWindow.h"
+
+extern void GuiMain();
 
 namespace vl
 {
 	namespace presentation
 	{
+		class IGuiSkin;
+		class IGuiSkinBuilder;
+		class GuiControl;
+		class GuiWindowBase;
+		class GuiApplication;
 
 /***********************************************************************
-基础设施
+皮肤
 ***********************************************************************/
 
-		class GuiBase : public Object
+		class IGuiSkin : public Interface
 		{
-		protected:
 		public:
+			virtual void					SetBounds(Rect value)=0;
+			virtual void					RemoveChild(IGuiSkin* child)=0;
+			virtual void					InsertChild(int index, IGuiSkin* child)=0;
+			virtual int						ChildCount()=0;
 		};
 
-		class GuiControl : public GuiBase
+		class IGuiSkinBuilder : public Interface
+		{
+		public:
+			virtual Ptr<IGuiSkin>			Build(INativeWindow* window)=0;
+		};
+
+/***********************************************************************
+控件
+***********************************************************************/
+
+		class GuiControl : public Object
 		{
 		public:
 			class Grid : public Object
 			{
+			private:
+				GuiControl*					ownedControl;
+			protected:
+			public:
+				Grid();
+				~Grid();
+
+				virtual GuiControl*			GetOwnedControl();
+				virtual bool				SetOwnedControl(GuiControl* control);
+
+				virtual int					GetChildCount()=0;
+				virtual GuiControl*			GetChild(int index)=0;
+				virtual int					GetChildIndex(GuiControl* child)=0;
 			};
+		private:
+			GuiControl*						parent;
+			Ptr<IGuiSkin>					skin;
+			WString							name;
+			Ptr<Grid>						container;
+			Rect							bounds;
+
 		protected:
+
+			virtual const WString&			GetSkinBuilderName()=0;
+			virtual void					NotifySetParent(GuiControl* value);
+			virtual void					NotifyAttachedToWindow(GuiWindowBase* window);
+			virtual void					NotifyChildEntering(GuiControl* value);
+			virtual void					NotifyChildEntered(GuiControl* value);
+			virtual void					NotifyChildLeaving(GuiControl* value);
+			virtual void					NotifyChildLeaved(GuiControl* value);
+			virtual void					NotifyMoving(Rect& value);
+			virtual void					NotifyMoved(Rect value);
 		public:
+			GuiControl();
+			~GuiControl();
+
+			GuiControl*						GetParent();
+
+			const WString&					GetControlName();
+			void							SetControlName(const WString& value);
+
+			Grid*							GetContainer();
+			void							SetContainer(Grid* value);
+
+			Rect							GetBounds();
+			void							SetBounds(Rect value);
 		};
 
-		class GuiWindowBase : public GuiBase
+		class GuiWindowBase : public GuiControl
 		{
 		protected:
 		public:
+			virtual INativeWindow*			GetContainingNativeWindow()=0;
 		};
+
+/***********************************************************************
+应用程序
+***********************************************************************/
+
+		class GuiApplication : public Object
+		{
+			typedef collections::Dictionary<WString, Ptr<IGuiSkinBuilder>>		SkinBuilderMap;
+		protected:
+			SkinBuilderMap					skinBuilders;
+		public:
+			GuiApplication();
+			~GuiApplication();
+
+			void							SetSkinBuilder(const WString& name, IGuiSkinBuilder* builder);
+			IGuiSkinBuilder*				GetSkinBuilder(const WString& name);
+		};
+
+		extern GuiApplication*				GetCurrentApplication();
 
 /***********************************************************************
 排版
