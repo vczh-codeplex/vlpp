1487c141cb92267aaaf5a9c6815fcc9f3b70c04f
vczh_cp <SND\vczh_cp>
2011-05-11 06:27:14 +0000

    Task library

--- Library/Entity/TaskEntity.cpp
+++ Library/Entity/TaskEntity.cpp
@@ -1,10 +1,12 @@
 #include "TaskEntity.h"
+#include "..\Exception.h"
 #include <Windows.h>
 
 namespace vl
 {
 	namespace entities
 	{
+		using namespace collections;
  
 /***********************************************************************
 ThreadPool
@@ -149,6 +151,191 @@ ThreadPool::Static
 		ThreadPool* ThreadPool::Current()
 		{
 			return currentThreadPool;
+		}
+ 
+/***********************************************************************
+CancellationToken
+***********************************************************************/
+
+		CancellationToken::CancellationToken()
+			:cancelled(false)
+		{
+		}
+
+		CancellationToken::~CancellationToken()
+		{
+		}
+
+		void CancellationToken::Cancel()
+		{
+			cancelled=true;
+		}
+
+		bool CancellationToken::IsCancellationRequested()
+		{
+			return cancelled;
+		}
+ 
+/***********************************************************************
+Task
+***********************************************************************/
+
+		void Task::Execute()
+		{
+			try
+			{
+				taskState=Running;
+				task();
+				taskExecutionEvent.Signal();
+				taskResult=token->IsCancellationRequested()?Cancelled:Finished;
+			}
+			catch(const Exception&)
+			{
+				taskResult=Failed;
+			}
+			taskState=Done;
+
+			SpinLock::Scope scope(taskLock);
+			switch(taskResult)
+			{
+			case Finished:
+				for(vint i=0;i<finishedTasks.Count();i++)
+				{
+					ThreadPool::Current()->Queue(finishedTasks[i].Cast<ITask>());
+				}
+				break;
+			case Cancelled:
+				for(vint i=0;i<cancelledTasks.Count();i++)
+				{
+					ThreadPool::Current()->Queue(cancelledTasks[i].Cast<ITask>());
+				}
+				break;
+			case Failed:
+				for(vint i=0;i<failedTasks.Count();i++)
+				{
+					ThreadPool::Current()->Queue(failedTasks[i].Cast<ITask>());
+				}
+				break;
+			}
+		}
+
+		Task::Task(CancellationToken* _token)
+			:taskState(Ready)
+			,taskResult(NotExecuted)
+			,token(_token)
+			,owningToken(_token!=0)
+		{
+			taskExecutionEvent.CreateManualUnsignal(false);
+		}
+
+		Task::Task(const Func<void()>& _task, CancellationToken* _token)
+			:taskState(Ready)
+			,taskResult(NotExecuted)
+			,token(_token)
+			,owningToken(_token!=0)
+		{
+			task=_task;
+		}
+
+		Task::Task(const Func<void(Task*)>& _task, CancellationToken* _token)
+			:taskState(Ready)
+			,taskResult(NotExecuted)
+			,token(_token)
+			,owningToken(_token!=0)
+		{
+			task=Curry(_task)(this);
+		}
+
+		Task::~Task()
+		{
+			if(owningToken)
+			{
+				delete token;
+			}
+		}
+
+		CancellationToken* Task::GetCancellationToken()
+		{
+			return token;
+		}
+
+		Task::TaskState Task::GetState()
+		{
+			return taskState;
+		}
+
+		Task::TaskResult Task::GetResult()
+		{
+			return taskResult;
+		}
+
+		void Task::ContinueWith(Ptr<Task> child, TaskResult conditions)
+		{
+			SpinLock::Scope scope(taskLock);
+			if(conditions&Finished)
+			{
+				finishedTasks.Add(child);
+			}
+			if(conditions&Cancelled)
+			{
+				cancelledTasks.Add(child);
+			}
+			if(conditions&Failed)
+			{
+				failedTasks.Add(child);
+			}
+			
+			if(taskState==Done && conditions&taskResult)
+			{
+				ThreadPool::Current()->Queue(child.Cast<ITask>());
+			}
+		}
+
+		void Task::Wait()
+		{
+			taskExecutionEvent.Wait();
+		}
+
+		bool Task::WaitAll(Task** tasks, vint count)
+		{
+			Array<WaitableObject*> objects(count);
+			for(vint i=0;i<count;i++)
+			{
+				objects[i]=&tasks[i]->taskExecutionEvent;
+			}
+			return WaitableObject::WaitAll(&objects[0], count);
+		}
+
+		bool Task::WaitAllForTime(Task** tasks, vint count, vint ms)
+		{
+			Array<WaitableObject*> objects(count);
+			for(vint i=0;i<count;i++)
+			{
+				objects[i]=&tasks[i]->taskExecutionEvent;
+			}
+			return WaitableObject::WaitAllForTime(&objects[0], count, ms);
+		}
+
+		vint Task::WaitAny(Task** tasks, vint count)
+		{
+			Array<WaitableObject*> objects(count);
+			for(vint i=0;i<count;i++)
+			{
+				objects[i]=&tasks[i]->taskExecutionEvent;
+			}
+			bool abandoned=false;
+			return WaitableObject::WaitAny(&objects[0], count, &abandoned);
+		}
+
+		vint Task::WaitAnyForTime(Task** tasks, vint count, vint ms)
+		{
+			Array<WaitableObject*> objects(count);
+			for(vint i=0;i<count;i++)
+			{
+				objects[i]=&tasks[i]->taskExecutionEvent;
+			}
+			bool abandoned=false;
+			return WaitableObject::WaitAnyForTime(&objects[0], count, ms, &abandoned);
 		}
 	}
 }
