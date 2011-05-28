33ff271f056e16b6019dcd1d682e49096e617b7b
vczh_cp <SND\vczh_cp>
2011-05-28 04:58:11 +0000

    ManagedX code generation skeleton code

---
+++ Library/Scripting/Languages/ManagedX/ManagedXCodeGenerator.h
@@ -0,0 +1,50 @@
+/***********************************************************************
+Vczh Library++ 3.0
+Developer: ³Âè÷å«(vczh)
+Scripting::Native X
+
+Classes:
+***********************************************************************/
+
+#ifndef VCZH_SCRIPTING_LANGUAGES_MANAGEDX_MANAGEDXCODEGENERATOR
+#define VCZH_SCRIPTING_LANGUAGES_MANAGEDX_MANAGEDXCODEGENERATOR
+
+#include "..\..\ManagedLanguage\ManagedLanguageExpression.h"
+#include "..\..\Common\AlgorithmDeclaration.h"
+#include "..\..\..\Stream\Accessor.h"
+
+namespace vl
+{
+	namespace scripting
+	{
+		namespace language_managedx
+		{
+			using namespace stream;
+			using namespace managedlanguage;
+
+			typedef struct ManagedXCodeGeneratorParameter
+			{
+				TextWriter&					writer;
+				vint						indentation;
+
+				ManagedXCodeGeneratorParameter(TextWriter& _writer, vint _indentation)
+					:writer(_writer)
+					,indentation(_indentation)
+				{
+				}
+			} MXCGP;
+
+			EXTERN_ALGORITHM_PROCEDURE(ManagedX_GenerateCode_Type, ManagedType, MXCGP)
+			EXTERN_ALGORITHM_PROCEDURE(ManagedX_GenerateCode_ExtendedType, ManagedExtendedType, MXCGP)
+			EXTERN_ALGORITHM_PROCEDURE(ManagedX_GenerateCode_Expression, ManagedExpression, MXCGP)
+			EXTERN_ALGORITHM_PROCEDURE(ManagedX_GenerateCode_ExtendedExpression, ManagedExtendedExpression, MXCGP)
+			EXTERN_ALGORITHM_PROCEDURE(ManagedX_GenerateCode_Statement, ManagedStatement, MXCGP)
+			EXTERN_ALGORITHM_PROCEDURE(ManagedX_GenerateCode_ExtendedStatement, ManagedExtendedStatement, MXCGP)
+			EXTERN_ALGORITHM_PROCEDURE(ManagedX_GenerateCode_Member, ManagedMember, MXCGP)
+			EXTERN_ALGORITHM_PROCEDURE(ManagedX_GenerateCode_Declaration, ManagedDeclaration, MXCGP)
+			EXTERN_ALGORITHM_PROCEDURE(ManagedX_GenerateCode_ExtendedDeclaration, ManagedExtendedDeclaration, MXCGP)
+		}
+	}
+}
+
+#endif
