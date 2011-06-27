595ab04c12f0283783c9fa63de5616169bce6c04
Zihan Chen <vczh@163.com>
2011-06-27 03:20:13 +0000

    ManagedSymbolManager

--- Library/Scripting/ManagedLanguage/ManagedLanguageSymbolManager.cpp
+++ Library/Scripting/ManagedLanguage/ManagedLanguageSymbolManager.cpp
@@ -6,6 +6,117 @@ namespace vl
 	{
 		namespace managedlanguage
 		{
+
+/***********************************************************************
+ManagedSymbolItem
+***********************************************************************/
+
+			ManagedSymbolItem::ManagedSymbolItem(ManagedSymbolManager* _manager)
+				:manager(_manager)
+				,parent(0)
+			{
+				manager->Register(this);
+			}
+
+			ManagedSymbolItem::~ManagedSymbolItem()
+			{
+			}
+
+			ManagedSymbolManager* ManagedSymbolItem::GetManager()
+			{
+				return manager;
+			}
+
+			ManagedSymbolItemGroup* ManagedSymbolItem::GetParent()
+			{
+				return parent;
+			}
+
+			const WString& ManagedSymbolItem::GetName()
+			{
+				return name;
+			}
+
+			const ManagedSymbolItem::IGroupMap& ManagedSymbolItem::ItemGroups()
+			{
+				return itemGroups.Wrap();
+			}
+
+			ManagedSymbolItemGroup* ManagedSymbolItem::ItemGroup(const WString& name)
+			{
+				vint index=itemGroups.Keys().IndexOf(name);
+				return index==-1?0:itemGroups.Values()[index];
+			}
+
+			void ManagedSymbolItem::Add(ManagedSymbolItem* item)
+			{
+				ManagedSymbolItemGroup* group=ItemGroup(item->GetName());
+				if(!group)
+				{
+					group=new ManagedSymbolItemGroup(this);
+					manager->Register(group);
+				}
+				group->Add(item);
+			}
+
+/***********************************************************************
+ManagedSymbolItemGroup
+***********************************************************************/
+
+			ManagedSymbolItemGroup::ManagedSymbolItemGroup(ManagedSymbolItem* _parent)
+				:parent(_parent)
+			{
+			}
+
+			ManagedSymbolItemGroup::~ManagedSymbolItemGroup()
+			{
+			}
+
+			ManagedSymbolItem* ManagedSymbolItemGroup::GetParent()
+			{
+				return parent;
+			}
+
+			const ManagedSymbolItemGroup::IItemList& ManagedSymbolItemGroup::Items()
+			{
+				return items.Wrap();
+			}
+
+			void ManagedSymbolItemGroup::Add(ManagedSymbolItem* item)
+			{
+				CHECK_ERROR(item->parent==0, L"ManagedSymbolItemGroup(ManagedSymbolItem*)#一个ManagedSymbolItem只能属于一个ManagedSymbolItemGroup。");
+				item->parent=this;
+				items.Add(item);
+			}
+
+/***********************************************************************
+ManagedSymbolItemGroup
+***********************************************************************/
+
+			ManagedSymbolManager::ManagedSymbolManager()
+			{
+				global=new ManagedSymbolItem(this);
+				global->name=L"global::";
+			}
+
+			ManagedSymbolManager::~ManagedSymbolManager()
+			{
+			}
+
+			void ManagedSymbolManager::Register(ManagedSymbolItem* item)
+			{
+				allocatedItems.Add(item);
+			}
+
+			void ManagedSymbolManager::Register(ManagedSymbolItemGroup* group)
+			{
+				allocatedGroups.Add(group);
+			}
+
+			ManagedSymbolItem* ManagedSymbolManager::Global()
+			{
+				return global;
+			}
 		}
 	}
 }
--- Library/Scripting/ManagedLanguage/ManagedLanguageSymbolManager.h
+++ Library/Scripting/ManagedLanguage/ManagedLanguageSymbolManager.h
@@ -17,6 +17,75 @@ namespace vl
 	{
 		namespace managedlanguage
 		{
+
+/***********************************************************************
+Basic Constructions
+***********************************************************************/
+
+			class ManagedSymbolItem : public Object
+			{
+				friend class ManagedSymbolItemGroup;
+				friend class ManagedSymbolManager;
+
+				typedef collections::Dictionary<WString, ManagedSymbolItemGroup*>			GroupMap;
+				typedef collections::IReadonlyDictionary<WString, ManagedSymbolItemGroup*>	IGroupMap;
+			protected:
+				ManagedSymbolManager*		manager;
+				ManagedSymbolItemGroup*		parent;
+				WString						name;
+				GroupMap					itemGroups;
+			public:
+				ManagedSymbolItem(ManagedSymbolManager* _manager);
+				~ManagedSymbolItem();
+
+				ManagedSymbolManager*		GetManager();
+				ManagedSymbolItemGroup*		GetParent();
+				const WString&				GetName();
+				const IGroupMap&			ItemGroups();
+				ManagedSymbolItemGroup*		ItemGroup(const WString& name);
+				void						Add(ManagedSymbolItem* item);
+			};
+
+			class ManagedSymbolItemGroup : public Object
+			{
+				typedef collections::List<ManagedSymbolItem*>								ItemList;
+				typedef collections::IReadonlyList<ManagedSymbolItem*>						IItemList;
+			protected:
+				ManagedSymbolItem*			parent;
+				ItemList					items;
+			public:
+				ManagedSymbolItemGroup(ManagedSymbolItem* _parent);
+				~ManagedSymbolItemGroup();
+
+				ManagedSymbolItem*			GetParent();
+				const IItemList&			Items();
+				void						Add(ManagedSymbolItem* item);
+			};
+
+/***********************************************************************
+Symbols
+***********************************************************************/
+
+/***********************************************************************
+ManagedSymbolManager
+***********************************************************************/
+
+			class ManagedSymbolManager : public Object
+			{
+				typedef collections::List<Ptr<ManagedSymbolItem>>							ItemList;
+				typedef collections::List<Ptr<ManagedSymbolItemGroup>>						GroupList;
+			protected:
+				ItemList					allocatedItems;
+				GroupList					allocatedGroups;
+				ManagedSymbolItem*			global;
+			public:
+				ManagedSymbolManager();
+				~ManagedSymbolManager();
+
+				void						Register(ManagedSymbolItem* item);
+				void						Register(ManagedSymbolItemGroup* group);
+				ManagedSymbolItem*			Global();
+			};
 		}
 	}
 }
