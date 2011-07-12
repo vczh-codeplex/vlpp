2c868459be8425482f45186171121230b933bdc8
Zihan Chen <vczh@163.com>
2011-07-12 14:48:53 +0000

    Texture cube

--- Candidate/Simulator/DirectX/Beginning/Beginning/Main.cpp
+++ Candidate/Simulator/DirectX/Beginning/Beginning/Main.cpp
@@ -100,19 +100,54 @@ public:
 				{D3DXVECTOR3( 1, -1, -1), D3DXVECTOR2(1, 1)},
 				{D3DXVECTOR3( 1,  1, -1), D3DXVECTOR2(1, 0)},
 				{D3DXVECTOR3(-1,  1, -1), D3DXVECTOR2(0, 0)},
+
+				{D3DXVECTOR3( 1, -1, -1), D3DXVECTOR2(0, 1)},
+				{D3DXVECTOR3( 1, -1,  1), D3DXVECTOR2(1, 1)},
+				{D3DXVECTOR3( 1,  1,  1), D3DXVECTOR2(1, 0)},
+				{D3DXVECTOR3( 1,  1, -1), D3DXVECTOR2(0, 0)},
+
+				{D3DXVECTOR3( 1, -1,  1), D3DXVECTOR2(0, 1)},
+				{D3DXVECTOR3(-1, -1,  1), D3DXVECTOR2(1, 1)},
+				{D3DXVECTOR3(-1,  1,  1), D3DXVECTOR2(1, 0)},
+				{D3DXVECTOR3( 1,  1,  1), D3DXVECTOR2(0, 0)},
+
+				{D3DXVECTOR3(-1, -1,  1), D3DXVECTOR2(0, 1)},
+				{D3DXVECTOR3(-1, -1, -1), D3DXVECTOR2(1, 1)},
+				{D3DXVECTOR3(-1,  1, -1), D3DXVECTOR2(1, 0)},
+				{D3DXVECTOR3(-1,  1,  1), D3DXVECTOR2(0, 0)},
+
+				{D3DXVECTOR3(-1,  1, -1), D3DXVECTOR2(0, 1)},
+				{D3DXVECTOR3( 1,  1, -1), D3DXVECTOR2(1, 1)},
+				{D3DXVECTOR3( 1,  1,  1), D3DXVECTOR2(1, 0)},
+				{D3DXVECTOR3(-1,  1,  1), D3DXVECTOR2(0, 0)},
+
+				{D3DXVECTOR3(-1, -1,  1), D3DXVECTOR2(0, 1)},
+				{D3DXVECTOR3( 1, -1,  1), D3DXVECTOR2(1, 1)},
+				{D3DXVECTOR3( 1, -1, -1), D3DXVECTOR2(1, 0)},
+				{D3DXVECTOR3(-1, -1, -1), D3DXVECTOR2(0, 0)},
 			};
 
 			unsigned int indices[] =
 			{
 				0, 3, 2,
 				0, 2, 1,
+				4, 7, 6,
+				4, 6, 5,
+				8, 11,10,
+				8, 10,9,
+				12,15,14,
+				12,14,13,
+				16,19,18,
+				16,18,17,
+				20,23,22,
+				20,22,21,
 			};
 
 			cube2.Fill(vertices, indices);
 
 			shader2.Fill(L"TextureVertexShader.txt", L"VShader", L"PShader")
 				.Field(L"POSITION", &TextureVertex::Position)
-				.Field(L"TEXCOORD0", &TextureVertex::Texcoord0)
+				.Field(L"TEXCOORD", &TextureVertex::Texcoord0)
 				;
 
 			texture.Update(L"TextureColumn.jpg");
