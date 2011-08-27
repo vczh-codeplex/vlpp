1fc44c3928031d49aeba72e76ebae222c245eed5
Zihan Chen <vczh@163.com>
2011-08-27 14:27:49 +0000

    Communicator software demo using VLPP Node Service

---
+++ Candidate/NodeService/Sample.Communication.Software/CommClient/MainForm.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace CommClient
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxServerName = new System.Windows.Forms.TextBox();
+            this.textBoxDisplayName = new System.Windows.Forms.TextBox();
+            this.buttonLogIn = new System.Windows.Forms.Button();
+            this.buttonChange = new System.Windows.Forms.Button();
+            this.textBoxMessage = new System.Windows.Forms.TextBox();
+            this.buttonSend = new System.Windows.Forms.Button();
+            this.textBoxMessages = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Server Name:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 42);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(83, 12);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Display Name:";
+            // 
+            // textBoxServerName
+            // 
+            this.textBoxServerName.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxServerName.Location = new System.Drawing.Point(122, 10);
+            this.textBoxServerName.Name = "textBoxServerName";
+            this.textBoxServerName.Size = new System.Drawing.Size(234, 21);
+            this.textBoxServerName.TabIndex = 2;
+            this.textBoxServerName.Text = "localhost/VczhCommunication";
+            // 
+            // textBoxDisplayName
+            // 
+            this.textBoxDisplayName.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxDisplayName.Location = new System.Drawing.Point(122, 39);
+            this.textBoxDisplayName.Name = "textBoxDisplayName";
+            this.textBoxDisplayName.Size = new System.Drawing.Size(234, 21);
+            this.textBoxDisplayName.TabIndex = 3;
+            // 
+            // buttonLogIn
+            // 
+            this.buttonLogIn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonLogIn.Location = new System.Drawing.Point(362, 8);
+            this.buttonLogIn.Name = "buttonLogIn";
+            this.buttonLogIn.Size = new System.Drawing.Size(75, 23);
+            this.buttonLogIn.TabIndex = 4;
+            this.buttonLogIn.Text = "Log In";
+            this.buttonLogIn.UseVisualStyleBackColor = true;
+            this.buttonLogIn.Click += new System.EventHandler(this.buttonLogIn_Click);
+            // 
+            // buttonChange
+            // 
+            this.buttonChange.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonChange.Location = new System.Drawing.Point(362, 37);
+            this.buttonChange.Name = "buttonChange";
+            this.buttonChange.Size = new System.Drawing.Size(75, 23);
+            this.buttonChange.TabIndex = 5;
+            this.buttonChange.Text = "Change";
+            this.buttonChange.UseVisualStyleBackColor = true;
+            this.buttonChange.Click += new System.EventHandler(this.buttonChange_Click);
+            // 
+            // textBoxMessage
+            // 
+            this.textBoxMessage.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxMessage.Location = new System.Drawing.Point(14, 518);
+            this.textBoxMessage.Name = "textBoxMessage";
+            this.textBoxMessage.Size = new System.Drawing.Size(342, 21);
+            this.textBoxMessage.TabIndex = 6;
+            // 
+            // buttonSend
+            // 
+            this.buttonSend.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonSend.Location = new System.Drawing.Point(362, 516);
+            this.buttonSend.Name = "buttonSend";
+            this.buttonSend.Size = new System.Drawing.Size(75, 23);
+            this.buttonSend.TabIndex = 7;
+            this.buttonSend.Text = "Send";
+            this.buttonSend.UseVisualStyleBackColor = true;
+            this.buttonSend.Click += new System.EventHandler(this.buttonSend_Click);
+            // 
+            // textBoxMessages
+            // 
+            this.textBoxMessages.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxMessages.Location = new System.Drawing.Point(14, 66);
+            this.textBoxMessages.Multiline = true;
+            this.textBoxMessages.Name = "textBoxMessages";
+            this.textBoxMessages.ReadOnly = true;
+            this.textBoxMessages.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBoxMessages.Size = new System.Drawing.Size(423, 444);
+            this.textBoxMessages.TabIndex = 8;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(449, 551);
+            this.Controls.Add(this.textBoxMessages);
+            this.Controls.Add(this.buttonSend);
+            this.Controls.Add(this.textBoxMessage);
+            this.Controls.Add(this.buttonChange);
+            this.Controls.Add(this.buttonLogIn);
+            this.Controls.Add(this.textBoxDisplayName);
+            this.Controls.Add(this.textBoxServerName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "MainForm";
+            this.Text = "Vczh Communication Client";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxServerName;
+        private System.Windows.Forms.TextBox textBoxDisplayName;
+        private System.Windows.Forms.Button buttonLogIn;
+        private System.Windows.Forms.Button buttonChange;
+        private System.Windows.Forms.TextBox textBoxMessage;
+        private System.Windows.Forms.Button buttonSend;
+        private System.Windows.Forms.TextBox textBoxMessages;
+    }
+}
+
---
+++ Candidate/NodeService/Sample.Communication.Software/CommServer/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NodeService.Endpoints;
+using NodeService;
+using System.Threading;
+using NodeService.Protocols;
+
+namespace CommServer
+{
+    class Program
+    {
+        public static List<Tuple<INodeEndpointProtocolServer, CommunicationService>> loggedInServices = new List<Tuple<INodeEndpointProtocolServer, CommunicationService>>();
+
+        public static void SendMessageToAll(string message)
+        {
+            Console.WriteLine(message);
+            List<CommunicationService> copied = null;
+            lock (loggedInServices)
+            {
+                for (int i = loggedInServices.Count - 1; i >= 0; i--)
+                {
+                    if (!loggedInServices[i].Item1.Connected)
+                    {
+                        loggedInServices.RemoveAt(i);
+                    }
+                }
+                copied = new List<CommunicationService>(loggedInServices.Select(s => s.Item2));
+            }
+
+            ThreadPool.QueueUserWorkItem((o) =>
+            {
+                foreach (var service in copied)
+                {
+                    service.Callback.OnMessage(message);
+                }
+            });
+        }
+
+        static void Main(string[] args)
+        {
+            INodeEndpointProtocolFactory factory = new NamedPipeProtocolFactory();
+            INodeEndpointProtocolServerListener listener = factory.CreateServerListener();
+
+            Thread serverThread = new Thread(() =>
+            {
+                while (true)
+                {
+                    CommunicationService service = new CommunicationService();
+                    INodeEndpointProtocolServer server = listener.WaitForServer("VczhCommunication", service);
+                    lock (loggedInServices)
+                    {
+                        loggedInServices.Add(Tuple.Create(server, service));
+                    }
+                    SendMessageToAll("Someone logged in");
+                }
+            });
+            serverThread.Start();
+
+            Console.WriteLine("Press [ENTER] to exit.");
+            Console.Read();
+
+            serverThread.Abort();
+        }
+    }
+
+    public interface ICommunicationCallback : INodeEndpointClient
+    {
+        string GetDisplayName();
+        void OnMessage(string message);
+    }
+
+    [NodeEndpoint("CommunicaitonService")]
+    public class CommunicationService : StrongTypedNodeEndpoint, IDuplexNodeEndpoint<ICommunicationCallback>
+    {
+        public ICommunicationCallback Callback { get; set; }
+
+        public CommunicationService()
+        {
+            this.EnableAsynchronization = true;
+        }
+
+        [NodeEndpointMethod]
+        public void Talk(string message)
+        {
+            Program.SendMessageToAll(Callback.GetDisplayName() + " says: " + message);
+        }
+
+        [NodeEndpointMethod]
+        public void ChangeDisplayName(string oldDisplayName)
+        {
+            Program.SendMessageToAll(oldDisplayName + " changed display name: " + Callback.GetDisplayName());
+        }
+    }
+}
