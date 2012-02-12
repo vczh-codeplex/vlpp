bd57a35015e645ec2c6c38ff86f3d048906354ea
Zihan Chen <vczh@163.com>
2012-02-12 14:34:17 +0000

    Codegen round 7

--- Tools/Release/SideProjects/GacUI/GacUI/DllMain.cpp
+++ Tools/Release/SideProjects/GacUI/GacUI/DllMain.cpp
@@ -1,6 +1,3 @@
-#include "GacUICpp.h"
-#include "..\..\..\..\..\Candidate\GUI\GUI\GacUI.h"
-#include "..\..\..\..\..\Candidate\GUI\GUI\Reflection\GuiTypeDescriptorImpProvider_codegen.h"
 #include <Windows.h>
 
 BOOL APIENTRY DllMain( HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved)
@@ -14,34 +11,4 @@ BOOL APIENTRY DllMain( HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReser
 		break;
 	}
 	return TRUE;
-}
-
-void (__stdcall*guimain_callback)()=0;
-
-void GuiMain()
-{
-	if(guimain_callback)
-	{
-		ITypeProvider* typeProvider=reflection_implementation::CreateDefaultTypeProvider();
-		SetTypeProvider(typeProvider);
-		guimain_callback();
-	}
-}
-
-extern "C"
-{
-	GACUI_API void SetGuiMain(void(__stdcall*guimain)())
-	{
-		guimain_callback=guimain;
-	}
-
-	GACUI_API void StartGDI()
-	{
-		SetupWindowsGDIRenderer();
-	}
-
-	GACUI_API void StartDirect2D()
-	{
-		SetupWindowsDirect2DRenderer();
-	}
 }
---
+++ Tools/Release/SideProjects/GacUI/_TranslateXMLtoCode/Codegen/Codegen_GacUI_codegen_h.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _TranslateXMLtoCode.Codegen
+{
+    class Codegen_GacUI_codegen_h : CodeGenerator
+    {
+        private CodeGeneratorOptions options;
+
+        protected void GenerateCppHeader()
+        {
+            List<string> classNames = new List<string>();
+            foreach (var udt in this.options.Udts)
+            {
+                int commonClassNames = 0;
+                for (int i = 0; i < udt.Name.Length; i++)
+                {
+                    if (i < classNames.Count && classNames[i] == udt.Name[i])
+                    {
+                        commonClassNames = i + 1;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+
+                for (int i = classNames.Count; i > commonClassNames; i--)
+                {
+                    End("};");
+                    WriteLine("");
+                    classNames.RemoveAt(i - 1);
+                }
+
+                WriteSectionComment(string.Format("{0}", udt.ToString()));
+                for (int i = commonClassNames; i < udt.Name.Length; i++)
+                {
+                    string className = udt.Name[i];
+                    classNames.Add(className);
+
+                    if (i == udt.Name.Length - 1 && udt.Kind == RgacUDTKind.Enum)
+                    {
+                        WriteLine("enum {0}", className);
+                        Begin("{");
+                        foreach (var item in udt.AssociatedGacType.Constants)
+                        {
+                            WriteLine("{0} = {1},", item.Name, item.EnumItemValue);
+                        }
+                    }
+                    else
+                    {
+                        WriteLine("class GACUI_API {0}", className);
+                        WriteLine("{");
+                        Begin("public:");
+                    }
+                }
+            }
+            for (int i = 0; i < classNames.Count; i++)
+            {
+                End("};");
+            }
+        }
+
+        protected override void GenerateCodeInternal()
+        {
+            WriteLine("#ifndef GACUI_CONTROL_LIBRARY_CPP");
+            WriteLine("#define GACUI_CONTROL_LIBRARY_CPP");
+            WriteLine("");
+            WriteLine("#include \"GacUICommon.h\"");
+            WriteLine("");
+            WriteLine("namespace gacui_cpp");
+            Begin("{");
+
+            GenerateCppHeader();
+
+            End("}");
+            WriteLine("");
+            WriteLine("namespace g{ using namespace gacui_cpp; }");
+            WriteLine("#endif");
+        }
+
+        public Codegen_GacUI_codegen_h(CodeGeneratorOptions options)
+            : base(options.GacuiPath + "GacUI.h")
+        {
+            this.options = options;
+        }
+    }
+}
