da1e8fce567d31ffac7ea25e8c61848d01dfe4ad
Zihan Chen <vczh@163.com>
2012-02-20 13:23:43 +0000

    Basic gate components

---
+++ Candidate/Games/LogicGateSimulator/Gate/BoardComponents/LogicChipInput.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+
+namespace Gate.BoardComponents
+{
+    class LogicChipInput : GateBoardComponent
+    {
+        public override Point[] Inputs
+        {
+            get
+            {
+                return new Point[0];
+            }
+        }
+
+        public override Point[] Outputs
+        {
+            get
+            {
+                Point o;
+                GatePainter.GetInputOutput(BoardEditorPanel.GridSize, this.Position, out o);
+                return new Point[] { o };
+            }
+        }
+
+        public override Rectangle Bounds
+        {
+            get
+            {
+                return GatePainter.GetInputBounds(BoardEditorPanel.GridSize, this.Position);
+            }
+        }
+
+        public override void Paint(Graphics g, Point offset, bool alert)
+        {
+            Point p = this.Position + new Size(offset);
+            GatePainter.PaintChipInput(g, BoardEditorPanel.GridSize, p, alert);
+        }
+    }
+}
---
+++ Candidate/Games/LogicGateSimulator/Gate/Tools/BoardEditorAddChipInputTool.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using Gate.BoardComponents;
+
+namespace Gate.Tools
+{
+    class BoardEditorAddChipInputTool : IBoardEditorTool
+    {
+        public Bitmap Icon
+        {
+            get
+            {
+                return ToolIcons.Input;
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                return "Chip Input";
+            }
+        }
+
+        public BoardEditorAddChipInputTool()
+        {
+        }
+
+        public IBoardEditorCommand CreateCommand()
+        {
+            return new BoardEditorComponentCommand(() => new LogicChipInput());
+        }
+    }
+}
