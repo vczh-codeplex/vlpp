6e316c66f274a0c892759fe815b2ea55352c2b96
Zihan Chen <vczh@163.com>
2012-03-20 20:26:08 +0000

    Build the document layout from the generated GacUISrcDoc.xml

--- Tools/Release/SideProjects/GacUISrc/DocRef/Program.cs
+++ Tools/Release/SideProjects/GacUISrc/DocRef/Program.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Xml.Linq;
+using System.IO;
 
 namespace DocRef
 {
@@ -11,6 +13,12 @@ namespace DocRef
         {
             if (args.Length == 2)
             {
+                XDocument docXml = XDocument.Load(args[0]);
+                string outputFolder = Path.GetFullPath(args[1]) + "\\";
+
+                var docRoot = DocItemAnalyzer.GetItems(docXml.Root);
+                Dictionary<string, DocItem> uniqueIdItemMap = new Dictionary<string, DocItem>();
+                docRoot.BuildIndex(uniqueIdItemMap);
             }
             else
             {
